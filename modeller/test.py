22ec0552c54f20cfa676b3db01da801112cd9a96
Erick Dransch <ekgdrans@uwaterloo.ca>
2013-11-17 20:03:28 -0500

    add diffuse lighting

--- modeller/test.py
+++ modeller/test.py
@@ -2,7 +2,6 @@
 
 from OpenGLContext import testingcontext
 from OpenGLContext.arrays import *
-from OpenGLContext.events.timer import Timer
 BaseContext = testingcontext.getInteractive()
 
 
@@ -11,21 +10,49 @@ from OpenGL.GL import shaders
 from OpenGL.arrays import vbo
 
 class TestContext(BaseContext):
-    """ creates a simple vertex shader """
+    """ Attribute types """
 
     def OnInit(self):
-        VERTEX_SHADER = shaders.compileShader("""
-        uniform float tween;
-        attribute vec3 position;
-        attribute vec3 tweened;
-        attribute vec3 color;
+
+        phong_calc = """
+        float phong_weightCalc(
+            in vec3 light_pos, // light position
+            in vec3 frag_normal // geometre normal
+        ){
+            // returns vec2( ambientMult, diffuseMult)
+            float n_dot_pos = max(0.0, dot(
+                frag_normal, light_pos
+                ));
+            return n_dot_pos;
+        }"""
+
+
+        VERTEX_SHADER = shaders.compileShader(phong_calc + """
+        uniform vec4 Global_ambient;
+        uniform vec4 Light_ambient;
+        uniform vec4 Light_diffuse;
+        uniform vec3 Light_location;
+        uniform vec4 Material_ambient;
+        uniform vec4 Material_diffuse;
+        attribute vec3 Vertex_position;
+        attribute vec3 Vertex_normal;
         varying vec4 baseColor;
         void main(){
-            gl_Position = gl_ModelViewProjectionMatrix * mix(
-                vec4(position, 1.0),
-                vec4(tweened, 1.0),
-                tween);
-            baseColor = vec4(color, 1.0);
+            gl_Position = gl_ModelViewProjectionMatrix * vec4( Vertex_position, 1.0);
+            vec3 EC_Light_location = gl_NormalMatrix * Light_location;
+            float diffuse_weight = phong_weightCalc(
+                normalize(EC_Light_location),
+                normalize(gl_NormalMatrix * Vertex_normal));
+            baseColor = clamp(
+            (
+                // global component
+                (Global_ambient * Material_ambient)
+                // material's interaction with light's contribution
+                // to the ambient lighting
+                + (Light_ambient * Material_ambient)
+                // material's interaction with direct light
+                + (Light_diffuse * Material_diffuse * diffuse_weight)
+                ), 0.0, 1.0);
         }""", GL_VERTEX_SHADER)
 
         FRAGMENT_SHADER = shaders.compileShader("""
@@ -38,63 +65,68 @@ class TestContext(BaseContext):
 
 
         self.vbo = vbo.VBO(array([
-                                [ 0,  1, 0,  1, 3, 0,  0,1,0],
-                                [-1, -1, 0, -1,-1, 0,  1, 1,0],
-                                [ 1, -1, 0,  1,-1, 0,  0,1,1],
-                                [ 2, -1, 0,  2,-1, 0,  1,0,0],
-                                [ 4, -1, 0,  4,-1, 0,  0,1,0],
-                                [ 4,  1, 0,  4, 9, 0,  0,0,1],
-                                [ 2, -1, 0,  2,-1, 0,  1,0,0],
-                                [ 4,  1, 0,  1, 3, 0,  0,0,1],
-                                [ 2,  1, 0,  1,-1, 0,  0,1,1],
+                                [-1, 0, 0,  -1, 0, 1],
+                                [ 0, 0, 1,  -1, 0, 2],
+                                [ 0, 1, 1,  -1, 0, 2],
+                                [-1, 0, 0,  -1, 0, 1],
+                                [ 0, 1, 1,  -1, 0, 2],
+                                [-1, 1, 0,  -1, 0, 1],
+                                [ 0, 0, 1,  -1, 0, 2],
+                                [ 1, 0, 1,   1, 0, 2],
+                                [ 1, 1, 1,   1, 0, 2],
+                                [ 0, 0, 1,  -1, 0, 2],
+                                [ 1, 1, 1,   1, 0, 2],
+                                [ 0, 1, 1,  -1, 0, 2],
+                                [ 1, 0, 1,   1, 0, 2],
+                                [ 2, 0, 0,   1, 0, 1],
+                                [ 2, 1, 0,   1, 0, 1],
+                                [ 1, 0, 1,   1, 0, 2],
+                                [ 2, 1, 0,   1, 0, 1],
+                                [ 1, 1, 1,   1, 0, 2],
                                 ], 'f'))
+        
+        for uniform in ( 'Global_ambient', 'Light_ambient', 'Light_diffuse',
+                        'Light_location', 'Material_ambient', 'Material_diffuse',):
+            location = glGetUniformLocation( self.shader, uniform)
+            if location in (None, -1):
+                print 'Warning, no uniform: %s' % (uniform)
+            setattr(self, uniform+'_loc', location)
 
-        self.position_location = glGetAttribLocation(self.shader, 'position')
-        self.tweened_location = glGetAttribLocation(self.shader, 'tweened')
-        self.color_location = glGetAttribLocation(self.shader, 'color')
-        self.tween_location = glGetUniformLocation(self.shader, 'tween')
+        for attribute in ( 'Vertex_position', 'Vertex_normal',):
+            location = glGetAttribLocation(self.shader, attribute)
+            if location in (None, -1):
+                print 'Warning, no attr: %s' % (uniform)
+            setattr(self, attribute+'_loc', location)
 
-        self.time = Timer(duration=2.0, repeating=1)
-        self.time.addEventHandler("fraction", self.OnTimerFraction)
-        self.time.register(self)
-        self.time.start()
-        self.tween_fraction = 0.0
-
-    def Render(self, mode):
+    def Render(self, mode=None):
         """ Render geometry """
         BaseContext.Render(self, mode)
         glUseProgram(self.shader)
-        glUniform1f(self.tween_location, self.tween_fraction)
         try:
             self.vbo.bind()
             try:
-                glEnableVertexAttribArray(self.position_location)
-                glEnableVertexAttribArray(self.tweened_location)
-                glEnableVertexAttribArray(self.color_location)
+                glUniform4f(self.Global_ambient_loc, .3, .05, .05, .1)
+                glUniform4f(self.Light_ambient_loc, .2, .2, .2, 1.0)
+                glUniform4f(self.Light_diffuse_loc, 1, 1, 1, 1)
+                glUniform3f(self.Light_location_loc, 2, 2, 10)
+                glUniform4f(self.Material_ambient_loc, 0.2, 0.2, 0.2, 1.0)
+                glUniform4f(self.Material_diffuse_loc, 1, 1, 1, 1.0)
 
-                stride = 9*4
-                glVertexAttribPointer( self.position_location, 3, GL_FLOAT, False, stride, self.vbo)
-                glVertexAttribPointer( self.tweened_location, 3, GL_FLOAT, False, stride, self.vbo+12)
-                glVertexAttribPointer( self.color_location, 3, GL_FLOAT, False, stride, self.vbo+24)
+                glEnableVertexAttribArray(self.Vertex_position_loc)
+                glEnableVertexAttribArray(self.Vertex_normal_loc)
 
-                glDrawArrays(GL_TRIANGLES, 0, 9)
+                stride = 6*4
+                glVertexAttribPointer( self.Vertex_position_loc, 3, GL_FLOAT, False, stride, self.vbo)
+                glVertexAttribPointer( self.Vertex_normal_loc, 3, GL_FLOAT, False, stride, self.vbo+12)
+
+                glDrawArrays(GL_TRIANGLES, 0, 18)
 
             finally:
                 self.vbo.unbind()
-                glDisableClientState(GL_VERTEX_ARRAY)
-                glDisableVertexAttribArray(self.position_location)
-                glDisableVertexAttribArray(self.tweened_location)
-                glDisableVertexAttribArray(self.color_location)
+                glDisableVertexAttribArray(self.Vertex_position_loc)
+                glDisableVertexAttribArray(self.Vertex_normal_loc)
         finally:
-            shaders.glUseProgram(0)
-
-    def OnTimerFraction(self, event):
-        frac = event.fraction()
-        if frac > 0.5:
-            frac = 1.0 - frac
-        frac *= 2
-        self.tween_fraction = frac
-        self.triggerRedraw()
+            glUseProgram(0)
 
 if __name__=="__main__":
     TestContext.ContextMainLoop()
