e72aec303ab49100fb5b239fc9cdd24b3ba45418
Erick Dransch <ekgdrans@uwaterloo.ca>
2013-12-08 19:53:51 -0500

    finish lighting tutorials

--- modeller/test.py
+++ modeller/test.py
@@ -2,6 +2,7 @@
 
 from OpenGLContext import testingcontext
 from OpenGLContext.arrays import *
+from OpenGLContext.scenegraph.basenodes import Sphere
 BaseContext = testingcontext.getInteractive()
 
 
@@ -15,78 +16,62 @@ class TestContext(BaseContext):
     def OnInit(self):
 
         phong_calc = """
-        float phong_weightCalc(
+        vec2 phong_weightCalc(
             in vec3 light_pos, // light position
-            in vec3 frag_normal // geometre normal
+            in vec3 half_light, // half-way vector between light and view
+            in vec3 frag_normal, // geometre normal
+            in float shininess
         ){
             // returns vec2( ambientMult, diffuseMult)
-            float n_dot_pos = max(0.0, dot(
-                frag_normal, light_pos
-                ));
-            return n_dot_pos;
+            float n_dot_pos = max(0.0, dot(frag_normal, light_pos));
+            float n_dot_half = 0.0;
+            if (n_dot_pos > -0.05){
+                n_dot_half = pow(max(0.0, dot(half_light, frag_normal)), shininess);
+            }
+            return vec2(n_dot_pos, n_dot_half);
         }"""
 
 
-        VERTEX_SHADER = shaders.compileShader(phong_calc + """
+        VERTEX_SHADER = shaders.compileShader("""
+        attribute vec3 Vertex_position;
+        attribute vec3 Vertex_normal;
+        varying vec3 baseNormal;
+        void main(){
+            gl_Position = gl_ModelViewProjectionMatrix * vec4( Vertex_position, 1.0);
+            baseNormal = gl_NormalMatrix * normalize(Vertex_normal);
+        }""", GL_VERTEX_SHADER)
+
+        FRAGMENT_SHADER = shaders.compileShader(phong_calc + """
         uniform vec4 Global_ambient;
         uniform vec4 Light_ambient;
         uniform vec4 Light_diffuse;
+        uniform vec4 Light_specular;
         uniform vec3 Light_location;
+        uniform float Material_shininess;
+        uniform vec4 Material_specular;
         uniform vec4 Material_ambient;
         uniform vec4 Material_diffuse;
-        attribute vec3 Vertex_position;
-        attribute vec3 Vertex_normal;
-        varying vec4 baseColor;
-        void main(){
-            gl_Position = gl_ModelViewProjectionMatrix * vec4( Vertex_position, 1.0);
-            vec3 EC_Light_location = gl_NormalMatrix * Light_location;
-            float diffuse_weight = phong_weightCalc(
-                normalize(EC_Light_location),
-                normalize(gl_NormalMatrix * Vertex_normal));
-            baseColor = clamp(
-            (
-                // global component
-                (Global_ambient * Material_ambient)
-                // material's interaction with light's contribution
-                // to the ambient lighting
-                + (Light_ambient * Material_ambient)
-                // material's interaction with direct light
-                + (Light_diffuse * Material_diffuse * diffuse_weight)
-                ), 0.0, 1.0);
-        }""", GL_VERTEX_SHADER)
+        varying vec3 baseNormal;
 
-        FRAGMENT_SHADER = shaders.compileShader("""
-        varying vec4 baseColor;
         void main(){
-            gl_FragColor = baseColor;
+            // normalized eye-coord Light location
+            vec3 EC_Light_location = normalize(gl_NormalMatrix * Light_location);
+            // half vector
+            vec3 Light_half = normalize(EC_Light_location - vec3(0,0,-1));
+            vec2 weights = phong_weightCalc( EC_Light_location, Light_half, baseNormal, Material_shininess);
+            gl_FragColor = clamp(((Global_ambient * Material_ambient) +
+                                  (Light_ambient * Material_ambient) +
+                                  (Light_diffuse * Material_diffuse * weights.x) +
+                                  (Light_specular * Material_specular * weights.y)), 0.0, 1.0);
         }""", GL_FRAGMENT_SHADER)
 
         self.shader = shaders.compileProgram(VERTEX_SHADER, FRAGMENT_SHADER)
 
 
-        self.vbo = vbo.VBO(array([
-                                [-1, 0, 0,  -1, 0, 1],
-                                [ 0, 0, 1,  -1, 0, 2],
-                                [ 0, 1, 1,  -1, 0, 2],
-                                [-1, 0, 0,  -1, 0, 1],
-                                [ 0, 1, 1,  -1, 0, 2],
-                                [-1, 1, 0,  -1, 0, 1],
-                                [ 0, 0, 1,  -1, 0, 2],
-                                [ 1, 0, 1,   1, 0, 2],
-                                [ 1, 1, 1,   1, 0, 2],
-                                [ 0, 0, 1,  -1, 0, 2],
-                                [ 1, 1, 1,   1, 0, 2],
-                                [ 0, 1, 1,  -1, 0, 2],
-                                [ 1, 0, 1,   1, 0, 2],
-                                [ 2, 0, 0,   1, 0, 1],
-                                [ 2, 1, 0,   1, 0, 1],
-                                [ 1, 0, 1,   1, 0, 2],
-                                [ 2, 1, 0,   1, 0, 1],
-                                [ 1, 1, 1,   1, 0, 2],
-                                ], 'f'))
+        self.coords, self.indices, self.count = Sphere(radius = 1).compile()
         
-        for uniform in ( 'Global_ambient', 'Light_ambient', 'Light_diffuse',
-                        'Light_location', 'Material_ambient', 'Material_diffuse',):
+        for uniform in ( 'Global_ambient', 'Light_ambient', 'Light_diffuse', 'Light_specular', 'Light_location', \
+                'Material_ambient', 'Material_diffuse', 'Material_shininess', 'Material_specular',):
             location = glGetUniformLocation( self.shader, uniform)
             if location in (None, -1):
                 print 'Warning, no uniform: %s' % (uniform)
@@ -103,26 +88,31 @@ class TestContext(BaseContext):
         BaseContext.Render(self, mode)
         glUseProgram(self.shader)
         try:
-            self.vbo.bind()
+            self.coords.bind()
+            self.indices.bind()
+            stride = self.coords.data[0].nbytes
             try:
-                glUniform4f(self.Global_ambient_loc, .3, .05, .05, .1)
-                glUniform4f(self.Light_ambient_loc, .2, .2, .2, 1.0)
-                glUniform4f(self.Light_diffuse_loc, 1, 1, 1, 1)
-                glUniform3f(self.Light_location_loc, 2, 2, 10)
-                glUniform4f(self.Material_ambient_loc, 0.2, 0.2, 0.2, 1.0)
-                glUniform4f(self.Material_diffuse_loc, 1, 1, 1, 1.0)
+                glUniform4f(self.Global_ambient_loc, .05, .05, .05, .1)
+                glUniform4f(self.Light_ambient_loc, .1, .1, .1, 1.0)
+                glUniform4f(self.Light_diffuse_loc, 0.25, 0.25, 0.25, 1)
+                glUniform4f(self.Light_specular_loc, 0.0, 1.0, 0.0, 1)
+                glUniform3f(self.Light_location_loc, 6, 2, 4)
+                glUniform4f(self.Material_ambient_loc, 0.1, 0.1, 0.1, 1.0)
+                glUniform4f(self.Material_diffuse_loc, 0.15, 0.15, 0.15, 1.0)
+                glUniform4f(self.Material_specular_loc, 1.0, 1.0, 1.0, 1.0)
+                glUniform1f(self.Material_shininess_loc, 0.95)
 
                 glEnableVertexAttribArray(self.Vertex_position_loc)
                 glEnableVertexAttribArray(self.Vertex_normal_loc)
 
-                stride = 6*4
-                glVertexAttribPointer( self.Vertex_position_loc, 3, GL_FLOAT, False, stride, self.vbo)
-                glVertexAttribPointer( self.Vertex_normal_loc, 3, GL_FLOAT, False, stride, self.vbo+12)
+                glVertexAttribPointer( self.Vertex_position_loc, 3, GL_FLOAT, False, stride, self.coords)
+                glVertexAttribPointer( self.Vertex_normal_loc, 3, GL_FLOAT, False, stride, self.coords+(5*4))
 
-                glDrawArrays(GL_TRIANGLES, 0, 18)
+                glDrawElements(GL_TRIANGLES, self.count, GL_UNSIGNED_SHORT, self.indices)
 
             finally:
-                self.vbo.unbind()
+                self.coords.unbind()
+                self.indices.unbind()
                 glDisableVertexAttribArray(self.Vertex_position_loc)
                 glDisableVertexAttribArray(self.Vertex_normal_loc)
         finally:
