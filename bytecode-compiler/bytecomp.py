740937b762adf91c898b5bf9ae89e2d5e3cd650d
Darius Bacon <darius@wry.me>
2013-12-11 02:15:00 -0300

    shambling to life

---
+++ bytecode-compiler/bytecomp.py
@@ -0,0 +1,78 @@
+"""
+Byte-compile trivial programs like "print(2+3)".
+"""
+
+import ast
+from collections import defaultdict
+from dis import dis, opmap 
+
+def make_table():
+    table = defaultdict(lambda: len(table))
+    return table
+
+def collect(table):
+    return tuple(sorted(table, key=table.get))
+
+class Struct:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+opc = Struct(**opmap)   # in Py3.4, opc = Enum('opc', opmap)
+
+def dummy(): pass
+Function = type(dummy)
+Code = type(dummy.__code__)
+
+def encode(u):
+    return (u % 256, u // 256)
+
+class CodeGen(ast.NodeVisitor):
+
+    def __init__(self):
+        self.constants = make_table()
+        self.names     = make_table()
+        self.varnames  = make_table()
+
+    def compile(self, node):
+        bytecode = self.visit(node)
+        bytecode += (opc.POP_TOP, opc.LOAD_CONST,) + encode(self.constants[None]) + (opc.RETURN_VALUE,)
+        argcount = 0
+        kwonlyargcount = 0
+        nlocals = 0
+        stacksize = 10          # XXX
+        flags = 67
+        return Code(argcount, kwonlyargcount, nlocals, stacksize, flags,
+                    bytes(bytecode),
+                    collect(self.constants),
+                    collect(self.names),
+                    collect(self.varnames),
+                    '<stdin>', 'the_name', 1, b'',
+                    freevars=(), cellvars=())
+
+    def visits(self, nodes):
+        return sum(map(self.visit, nodes), ())
+
+    def visit_Num(self, node):
+        return (opc.LOAD_CONST,) + encode(self.constants[node.n])
+
+    def visit_Name(self, node):
+        return (opc.LOAD_GLOBAL,) + encode(self.names[node.id])  # XXX LOAD_NAME in general
+
+    def visit_BinOp(self, node):
+        binops = {ast.Add: opc.BINARY_ADD}
+        return self.visit(node.left) + self.visit(node.right) + (binops[type(node.op)],)
+
+    def visit_Call(self, node):
+        return (self.visit(node.func) + self.visits(node.args) + (opc.CALL_FUNCTION,) + encode(len(node.args)))
+
+    def visit_Expr(self, node):
+        return self.visit(node.value)
+
+    def visit_Module(self, node):
+        return self.visits(node.body)
+
+ast5 = ast.parse("print(2+3)")
+code5 = CodeGen().compile(ast5)
+dis(code5)
+
+f = Function(code5, globals())
+f()   # It's alive!
