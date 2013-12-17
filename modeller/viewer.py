0a5ccf1a8dc4178e4687be17ff1dbceacb82e61b
Erick Dransch <ekgdrans@uwaterloo.ca>
2013-12-16 21:00:18 -0500

    begin work on modeller. Show grid

---
+++ modeller/viewer.py
@@ -0,0 +1,106 @@
+#! /usr/bin/env python
+
+from OpenGLContext import testingcontext
+from OpenGLContext.arrays import *
+from OpenGLContext.scenegraph.basenodes import Sphere
+BaseContext = testingcontext.getInteractive()
+
+from OpenGL.GL import *
+from OpenGL.GL import shaders
+from OpenGL.GLU import *
+from OpenGL.arrays import vbo
+OBJ_PLANE = 1
+
+class TestContext(BaseContext):
+    def OnInit(self):
+        self.MakePlane()
+        pass
+    def Lights(self, mode = None):
+        """ called by default rendering process iff there is no scenegraph present """
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
+        print "LIGHTING"
+
+    def MakePlane(self):
+        glNewList(OBJ_PLANE, GL_COMPILE)
+        glBegin(GL_LINES)
+        glColor3d(0,0,0)
+        for i in xrange(41):
+            glVertex3f(-10.0 + 0.5 * i, 0, -10)
+            glVertex3f(-10.0 + 0.5 * i, 0, 10)
+            glVertex3f(-10.0 , 0, -10 + 0.5 * i)
+            glVertex3f(10.0 , 0, -10 + 0.5 * i)
+
+        # Axes
+        glEnd()
+        glLineWidth(5)
+
+        glBegin(GL_LINES)
+        glColor3f(0.5, 0.7, 0.5)
+        glVertex3f(0.0, 0.0, 0.0)
+        glVertex3f(5, 0.0, 0.0)
+        glEnd()
+
+        glBegin(GL_LINES)
+        glColor3f(0.5, 0.7, 0.5)
+        glVertex3f(0.0, 0.0, 0.0)
+        glVertex3f(0.0, 5, 0.0)
+        glEnd()
+
+        glBegin(GL_LINES)
+        glColor3f(0.5, 0.7, 0.5)
+        glVertex3f(0.0, 0.0, 0.0)
+        glVertex3f(0.0, 0.0, 5)
+        glEnd()
+
+        # Draw the Y.
+        glBegin(GL_LINES)
+        glColor3f(0.0, 0.0, 0.0)
+        glVertex3f(0.0, 5.0, 0.0)
+        glVertex3f(0.0, 5.5, 0.0)
+        glVertex3f(0.0, 5.5, 0.0)
+        glVertex3f(-0.5, 6.0, 0.0)
+        glVertex3f(0.0, 5.5, 0.0)
+        glVertex3f(0.5, 6.0, 0.0)
+
+        # Draw the Z.
+        glVertex3f(-0.5, 0.0, 5.0)
+        glVertex3f(0.5, 0.0, 5.0)
+        glVertex3f(0.5, 0.0, 5.0)
+        glVertex3f(-0.5, 0.0, 6.0)
+        glVertex3f(-0.5, 0.0, 6.0)
+        glVertex3f(0.5, 0.0, 6.0)
+
+        # Draw the X.
+        glVertex3f(5.0, 0.0, 0.5)
+        glVertex3f(6.0, 0.0, -0.5)
+        glVertex3f(5.0, 0.0, -0.5)
+        glVertex3f(6.0, 0.0, 0.5)
+
+        glEnd()
+        glLineWidth(1)
+        glEndList()
+
+    def Render(self, mode = None):
+        BaseContext.Render(self, mode)
+
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
+        glLightfv(GL_LIGHT0, GL_POSITION, GLfloat_4(0, 15, -15, 1))
+        glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, GLfloat_3(0, -1, 1))
+
+        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
+        glEnable( GL_COLOR_MATERIAL)
+
+        glClearColor(0.4, 0.4, 0.4, 0.0);
+        glClear(GL_COLOR_BUFFER_BIT)
+        gluLookAt(0, 10, -10,
+                    0, 0, 0,
+                    0, 1, 0)
+
+
+        glCallList(OBJ_PLANE)
+#        self.MakePlane()
+
+if __name__=="__main__":
+    TestContext.ContextMainLoop()
