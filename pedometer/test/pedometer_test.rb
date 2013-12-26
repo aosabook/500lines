992abc6cc8867dd179d3da35a1b909f0a12c6c0a
Dessy Daskalov <dessy.daskalov@gmail.com>
2013-12-26 17:11:40 -0500

    Very basic pedometer

---
+++ pedometer/test/pedometer_test.rb
@@ -0,0 +1,64 @@
+require 'test/unit'
+require 'pedometer'
+
+class PedometerTest < Test::Unit::TestCase
+
+  def test_create
+    input = "x,y,z;0.123,-0.123,5;"
+    pedometer = Pedometer.new(input)
+    
+    assert_equal 0, pedometer.steps
+    assert_equal 0, pedometer.distance
+    assert_equal input, pedometer.raw_data
+    assert_equal [[0.123, 0.123, 5.0]], pedometer.parsed_data
+  end
+
+  def test_create_no_input
+    flunk
+  end
+
+  def test_create_bad_input
+    flunk
+  end
+
+  def test_count_steps
+    pedometer = Pedometer.new(File.read('test/data/results-0-steps.txt'))
+    pedometer.count_steps
+    assert_equal 0, pedometer.steps
+
+    pedometer = Pedometer.new(File.read('test/data/results-15-steps.txt'))
+    pedometer.count_steps
+    assert_equal 15, pedometer.steps
+  end
+
+  def test_count_distance_before_steps
+    pedometer = Pedometer.new(File.read('test/data/results-0-steps.txt'))
+    pedometer.count_distance
+    assert_equal 0, pedometer.distance
+    
+    pedometer = Pedometer.new(File.read('test/data/results-15-steps.txt'))
+    pedometer.count_distance
+    assert_equal 0, pedometer.distance
+  end
+
+  def test_count_distance_after_steps_steps
+    pedometer = Pedometer.new(File.read('test/data/results-0-steps.txt'))
+    pedometer.count_steps
+    pedometer.count_distance
+    assert_equal 0, pedometer.distance
+    
+    pedometer = Pedometer.new(File.read('test/data/results-15-steps.txt'))
+    pedometer.count_steps
+    pedometer.count_distance
+    assert_equal 13.5, pedometer.distance
+  end
+
+  def test_results
+    pedometer = Pedometer.new(File.read('test/data/results-0-steps.txt'))
+    assert_equal ({ :steps => 0, :distance => 0}), pedometer.results
+
+    pedometer = Pedometer.new(File.read('test/data/results-15-steps.txt'))
+    assert_equal ({ :steps => 15, :distance => 13.5}), pedometer.results
+  end
+
+end
