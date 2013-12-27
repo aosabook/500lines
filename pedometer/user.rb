7a247227f5335e328968e1f56e5c4732702ee702
Dessy Daskalov <dessy.daskalov@gmail.com>
2013-12-26 20:10:21 -0500

    Clean up

--- pedometer/user.rb
+++ pedometer/user.rb
@@ -1,33 +1,34 @@
 class User
 
+  # TODO: Limit gender and system to specific values
   attr_accessor :gender, :height, :stride, :system
 
-def initialize(params)
-  params ||= {}
+  def initialize(params)
+    params ||= {}
 
-  @gender = params[:gender]
-  @height = params[:height]
-  @stride = params[:stride] || calculate_stride
-  @system = params[:system] || 'metric'
-end
-
-def calculate_stride
-  # avg: 74 cm
-  # avg male: 78 cm
-  # avg female: 70 cm
-  # male height * .415
-  # female height * .413
-
-  if @height && @gender
-    multiplier = (@gender == 'male') ? 0.415 : 0.413
-    @height * multiplier    
-  elsif @height
-    @height * 0.415
-  elsif @gender
-    (@gender == 'male') ? 78 : 70
-  else
-    74
+    @gender = params[:gender]
+    @height = params[:height]
+    @stride = params[:stride] || calculate_stride
+    @system = params[:system] || 'metric'
   end
-end
+
+  def calculate_stride
+    # avg: 74 cm
+    # avg male: 78 cm
+    # avg female: 70 cm
+    # male height * .415
+    # female height * .413
+
+    if @height && @gender
+      multiplier = (@gender == 'male') ? 0.415 : 0.413
+      @height * multiplier    
+    elsif @height
+      @height * 0.415
+    elsif @gender
+      (@gender == 'male') ? 78 : 70
+    else
+      74
+    end
+  end
 
 end
