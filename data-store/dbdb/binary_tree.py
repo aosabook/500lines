6e51f181f507e85bfa9e57e2c112f6f38cb8b62a
Taavi Burns <taavi.burns@points.com>
2013-12-29 16:57:13 -0500

    Rewrites the binary tree in terms of the tree container and nodes. Definitely cleaner.

--- data-store/dbdb/binary_tree.py
+++ data-store/dbdb/binary_tree.py
@@ -5,66 +5,89 @@
 # Maybe with mutability when actualizing persistence.
 
 class BinaryTree(object):
-    _EMPTY = object()
+    def __init__(self):
+        self._tree = None
 
-    def __init__(self, key=_EMPTY, value=None):
-        self._key = key
-        self._value = value
-        self._left = None
-        self._right = None
+    def get(self, key):
+        node = self._tree
+        while node is not None:
+            if key < node.key:
+                node = node.left
+            elif node.key < key:
+                node = node.right
+            else:
+                return node.value
+        raise KeyError
 
-    def __getitem__(self, key):
-        if key < self._key:
-            if self._left:
-                return self._left[key]
+    def set_tree(self, new_tree):
+        self._tree = new_tree
+
+    def set(self, key, value):
+        setter = self.set_tree
+        node = self._tree
+
+        while node is not None:
+            if key < node.key:
+                setter = node.set_left
+                node = node.left
+            elif node.key < key:
+                setter = node.set_right
+                node = node.right
             else:
-                raise KeyError
-        elif self._key < key:
-            if self._right:
-                return self._right[key]
-            else:
-                raise KeyError
+                break
+
+        if node:
+            # Found a node, therefore it's a value overwrite
+            node.value = value
         else:
-            return self._value
+            setter(BinaryNode(None, key, value, None))
 
-    def __setitem__(self, key, value):
-        if self._key is self._EMPTY:
-            self._key = key
-            self._value = value
-        elif key < self._key:
-            if self._left:
-                self._left[key] = value
+    def pop(self, key):
+        setter = self.set_tree
+        node = self._tree
+
+        while node is not None:
+            if key < node.key:
+                setter = node.set_left
+                node = node.left
+            elif node.key < key:
+                setter = node.set_right
+                node = node.right
             else:
-                self._left = type(self)(key, value)
-        elif self._key < key:
-            if self._right:
-                self._right[key] = value
-            else:
-                self._right = type(self)(key, value)
-        else:
-            self._value = value
+                if not node.left and not node.right:
+                    setter(None)
+                elif node.left and not node.right:
+                    setter(node.left)
+                elif not node.left and node.right:
+                    setter(node.right)
+                else:
+                    raise NotImplementedError
+                return
 
-    def __delitem__(self, key):
-        raise NotImplementedError
-
-    # These are all optional
-
-    def __contains__(self, key):
-        pass
-
-    def __len__(self):
-        pass
-
-    def __iter__(self):
-        pass
-
-    def __reversed__(self):
-        pass
+        raise KeyError
 
     def __str__(self):
-        return str({
-            (self._key, self._value): [
-                str(self._left),
-                str(self._right),
-            ],
-        })
+        import pprint
+        return "BinaryTree<\n{}\n>".format(
+            pprint.pformat(self._tree and self._tree.to_dict())
+        )
+
+
+class BinaryNode(object):
+    def __init__(self, left, key, value, right):
+        self.left = left
+        self.key = key
+        self.value = value
+        self.right = right
+
+    def set_left(self, new_left):
+        self.left = new_left
+
+    def set_right(self, new_right):
+        self.right = new_right
+
+    def to_dict(self):
+        return {(self.key, self.value): {
+            'left': self.left and self.left.to_dict(),
+            'right': self.right and self.right.to_dict(),
+        }}
--- data-store/dbdb/tests/test_binary_tree.py
+++ data-store/dbdb/tests/test_binary_tree.py
@@ -1,3 +1,5 @@
+import random
+
 from nose.tools import assert_raises, eq_
 
 from dbdb.binary_tree import BinaryTree
@@ -7,22 +9,65 @@ class TestBinaryTree(object):
     def test_get_missing_key_raises_key_error(self):
         tree = BinaryTree()
         with assert_raises(KeyError):
-            tree['Not A Key In The Tree']
+            tree.get('Not A Key In The Tree')
 
     def test_set_and_get_key(self):
         tree = BinaryTree()
-        tree['a'] = 'b'
-        eq_(tree['a'], 'b')
+        tree.set('a', 'b')
+        eq_(tree.get('a'), 'b')
+
+    def test_random_set_and_get_keys(self):
+        tree = BinaryTree()
+        ten_k = list(range(10000))
+        pairs = zip(random.sample(ten_k, 10), random.sample(ten_k, 10))
+        for k, v in pairs:
+            tree.set(k, v)
+        for k, v in pairs:
+            eq_(tree.get(k), v)
 
     def test_overwrite_and_get_key(self):
         tree = BinaryTree()
-        tree['a'] = 'b'
-        tree['a'] = 'c'
-        eq_(tree['a'], 'c')
+        tree.set('a', 'b')
+        tree.set('a', 'c')
+        eq_(tree.get('a'), 'c')
 
-    def test_del_key(self):
+    def test_pop_non_existent_key(self):
         tree = BinaryTree()
-        tree['a'] = 'b'
-        del tree['a']
         with assert_raises(KeyError):
-            tree['a']
+            tree.pop('Not A Key In The Tree')
+
+    def test_del_leaf_key(self):
+        tree = BinaryTree()
+        tree.set('b', '2')
+        tree.pop('b')
+        with assert_raises(KeyError):
+            tree.get('b')
+
+    def test_del_left_node_key(self):
+        tree = BinaryTree()
+        tree.set('b', '2')
+        tree.set('a', '1')
+        tree.pop('b')
+        with assert_raises(KeyError):
+            tree.get('b')
+        tree.get('a')
+
+    def test_del_right_node_key(self):
+        tree = BinaryTree()
+        tree.set('b', '2')
+        tree.set('c', '3')
+        tree.pop('b')
+        with assert_raises(KeyError):
+            tree.get('b')
+        tree.get('c')
+
+    def test_del_full_node_key(self):
+        tree = BinaryTree()
+        tree.set('b', '2')
+        tree.set('a', '1')
+        tree.set('c', '3')
+        tree.pop('b')
+        with assert_raises(KeyError):
+            tree.get('b')
+        tree.get('a')
+        tree.get('c')
