9d3ff6a5c91be2f4cfae9225f38d4e6aaf5a046a
Taavi Burns <taavi.burns@points.com>
2013-12-29 10:16:26 -0500

    Fleshing out the binary tree. Read/insert only for now.

--- data-store/dbdb/binary_tree.py
+++ data-store/dbdb/binary_tree.py
@@ -1,6 +1,70 @@
+# TODO: Leave the getitem and setitem stuff to the db wrapper. We need a richer
+# interface at this level.
+
+# Here, we can also make it something closer to a persistent data structure.
+# Maybe with mutability when actualizing persistence.
+
 class BinaryTree(object):
+    _EMPTY = object()
+
+    def __init__(self, key=_EMPTY, value=None):
+        self._key = key
+        self._value = value
+        self._left = None
+        self._right = None
+
     def __getitem__(self, key):
-        pass
+        if key < self._key:
+            if self._left:
+                return self._left[key]
+            else:
+                raise KeyError
+        elif self._key < key:
+            if self._right:
+                return self._right[key]
+            else:
+                raise KeyError
+        else:
+            return self._value
 
     def __setitem__(self, key, value):
+        if self._key is self._EMPTY:
+            self._key = key
+            self._value = value
+        elif key < self._key:
+            if self._left:
+                self._left[key] = value
+            else:
+                self._left = type(self)(key, value)
+        elif self._key < key:
+            if self._right:
+                self._right[key] = value
+            else:
+                self._right = type(self)(key, value)
+        else:
+            self._value = value
+
+    def __delitem__(self, key):
+        raise NotImplementedError
+
+    # These are all optional
+
+    def __contains__(self, key):
         pass
+
+    def __len__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __reversed__(self):
+        pass
+
+    def __str__(self):
+        return str({
+            (self._key, self._value): [
+                str(self._left),
+                str(self._right),
+            ],
+        })
--- data-store/dbdb/tests/test_binary_tree.py
+++ data-store/dbdb/tests/test_binary_tree.py
@@ -4,7 +4,6 @@ from dbdb.binary_tree import BinaryTree
 
 
 class TestBinaryTree(object):
-
     def test_get_missing_key_raises_key_error(self):
         tree = BinaryTree()
         with assert_raises(KeyError):
@@ -14,3 +13,16 @@ class TestBinaryTree(object):
         tree = BinaryTree()
         tree['a'] = 'b'
         eq_(tree['a'], 'b')
+
+    def test_overwrite_and_get_key(self):
+        tree = BinaryTree()
+        tree['a'] = 'b'
+        tree['a'] = 'c'
+        eq_(tree['a'], 'c')
+
+    def test_del_key(self):
+        tree = BinaryTree()
+        tree['a'] = 'b'
+        del tree['a']
+        with assert_raises(KeyError):
+            tree['a']
