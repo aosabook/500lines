ac6f635704bdf189bda49e11ff4169beef88e0e7
Dessy Daskalov <dessy.daskalov@gmail.com>
2013-12-30 21:52:36 -0500

    Add measure_time method to Pedometer

--- pedometer/models/pedometer.rb
+++ pedometer/models/pedometer.rb
@@ -3,12 +3,14 @@ class Pedometer
   CAP = 1.2
 
   attr_reader :raw_data, :parsed_data
-  attr_reader :steps, :distance
+  attr_reader :steps, :distance, :time, :interval
 
   def initialize(mobile_output)
     @raw_data = mobile_output
     @steps    = 0
     @distance = 0
+    @time     = 0
+    @interval = 'seconds'
 
     verify_data_format
     parse_raw_data
@@ -23,7 +25,7 @@ class Pedometer
 
   def measure
     measure_steps
-    measure_distance    
+    measure_distance
   end
 
   def measure_steps
@@ -38,12 +40,29 @@ class Pedometer
     @distance = stride_length * @steps
   end
 
+  def measure_time
+    # TODO: Get sampling rate from user object
+    sampling_rate = 5.0 # samples/second
+    seconds = @parsed_data.count/sampling_rate
+    
+    if seconds > 3600
+      @time = (seconds/3600).round(2)
+      @interval = 'hours'
+    elsif seconds > 60
+      @time = (seconds/60).round(2)
+      @interval = 'minutes'
+    else
+      @time = seconds.round(2)
+      @interval = 'seconds'
+    end
+  end
+
   # -- Data Manipulation ----------------------------------------------------
 
   def verify_data_format
     regexp = Regexp.new(/^((-?\d+(?:\.\d+)?,){2}-?\d+(?:\.\d+)?;)+$/)
     unless regexp.match(@raw_data)
-      raise "Bad Input. Ensure data is a series of comma separated x,y,z coordiantes separated by semi colons."
+      raise "Bad Input. Ensure data is a series of comma separated x,y,z coordiantes separated by semicolons."
     end
   end
 
--- pedometer/test/unit/pedometer_test.rb
+++ pedometer/test/unit/pedometer_test.rb
@@ -11,40 +11,44 @@ class PedometerTest < Test::Unit::TestCase
     
     assert_equal 0, pedometer.steps
     assert_equal 0, pedometer.distance
+
+    assert_equal 0, pedometer.time
+    assert_equal 'seconds', pedometer.interval
+
     assert_equal input, pedometer.raw_data
     assert_equal [[0.123, 0.123, 5.0]], pedometer.parsed_data
   end
 
   def test_create_nil_input
-    message = "Bad Input. Ensure data is a series of comma separated x,y,z coordiantes separated by semi colons."
+    message = "Bad Input. Ensure data is a series of comma separated x,y,z coordiantes separated by semicolons."
     assert_raise_with_message(RuntimeError, message) do
       Pedometer.new(nil)      
     end
   end
 
   def test_create_empty_input
-    message = "Bad Input. Ensure data is a series of comma separated x,y,z coordiantes separated by semi colons."
+    message = "Bad Input. Ensure data is a series of comma separated x,y,z coordiantes separated by semicolons."
     assert_raise_with_message(RuntimeError, message) do
       Pedometer.new('')
     end
   end
 
   def test_create_bad_input_strings
-    message = "Bad Input. Ensure data is a series of comma separated x,y,z coordiantes separated by semi colons."
+    message = "Bad Input. Ensure data is a series of comma separated x,y,z coordiantes separated by semicolons."
     assert_raise_with_message(RuntimeError, message) do
       Pedometer.new("0.123,-0.123,5;a,b,c;")
     end
   end
 
   def test_create_bad_input_too_many_values
-    message = "Bad Input. Ensure data is a series of comma separated x,y,z coordiantes separated by semi colons."
+    message = "Bad Input. Ensure data is a series of comma separated x,y,z coordiantes separated by semicolons."
     assert_raise_with_message(RuntimeError, message) do
       Pedometer.new("0.123,-0.123,5;0.123,-0.123,5,9;")
     end
   end
 
   def test_create_bad_input_too_few_values
-    message = "Bad Input. Ensure data is a series of comma separated x,y,z coordiantes separated by semi colons."
+    message = "Bad Input. Ensure data is a series of comma separated x,y,z coordiantes separated by semicolons."
     assert_raise_with_message(RuntimeError, message) do
       Pedometer.new("0.123,-0.123,5;0.123,-0.123;")
     end
@@ -82,6 +86,29 @@ class PedometerTest < Test::Unit::TestCase
     pedometer.measure_steps
     pedometer.measure_distance
     assert_equal 0.0135, pedometer.distance
+  end
+
+  def test_measure_time_seconds
+    pedometer = Pedometer.new(File.read('test/data/results-15-steps.txt'))
+    pedometer.measure_time
+    assert_equal 5.8, pedometer.time
+    assert_equal 'seconds', pedometer.interval
+  end
+
+  def measure_time_minutes
+    # Fake out 1000 samples
+    pedometer = Pedometer.new(1000.times.inject('') {|a| a+='1,1,1;';a})
+    pedometer.measure_time
+    assert_equal 3.33, pedometer.time
+    assert_equal 'minutes', pedometer.interval
+  end
+
+  def measure_time_hours
+    # Fake out 20000 samples
+    pedometer = Pedometer.new(20000.times.inject('') {|a| a+='1,1,1;';a})
+    pedometer.measure_time
+    assert_equal 1.11, pedometer.time
+    assert_equal 'hours', pedometer.interval
   end
 
   def test_measure
