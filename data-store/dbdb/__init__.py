dbe471f073fc4ff7d38caea7bf3c2ee3d50eaf6d
Taavi Burns <taavi.burns@points.com>
2014-01-03 14:34:47 -0500

    Removes unused :memory: target. If you want to try stubbing things or using your own fh, use DBDB(f) directly.

--- data-store/dbdb/__init__.py
+++ data-store/dbdb/__init__.py
@@ -1,20 +1,15 @@
-try:
-    from cStringIO import StringIO
-except ImportError:
-    import StringIO
-
 import os
 
 from dbdb.interface import DBDB
 
 
+__all__ = ['DBDB', 'connect']
+
+
 def connect(dbname):
-    if dbname == ':memory:':
-        f = StringIO()
-    else:
-        try:
-            f = open(dbname, 'r+b')
-        except IOError:
-            fd = os.open(dbname, os.O_RDWR | os.O_CREAT)
-            f = os.fdopen(fd, 'r+b')
+    try:
+        f = open(dbname, 'r+b')
+    except IOError:
+        fd = os.open(dbname, os.O_RDWR | os.O_CREAT)
+        f = os.fdopen(fd, 'r+b')
     return DBDB(f)
