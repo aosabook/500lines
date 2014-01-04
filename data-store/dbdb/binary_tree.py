1c8bae276d9bc000cfba46fa90ba056983a54dfe
Taavi Burns <taavi.burns@points.com>
2014-01-04 00:08:32 -0500

    Extracts the non-binary parts of the BinaryTree into a base class, again making it obvious how things are put together.

--- data-store/dbdb/binary_tree.py
+++ data-store/dbdb/binary_tree.py
@@ -4,115 +4,7 @@ except ImportError:
     import pickle
 
 
-from dbdb.refs import NodeRef
-
-
-class BinaryTree(object):
-    def __init__(self, storage):
-        self._storage = storage
-        self._refresh_tree_ref()
-
-    def commit(self):
-        self._tree_ref.store(self._storage)
-        self._storage.commit_root_address(self._tree_ref.address)
-
-    def _refresh_tree_ref(self):
-        self._tree_ref = BinaryNodeRef(
-            address=self._storage.get_root_address())
-
-    def get(self, key):
-        if not self._storage.locked:
-            self._refresh_tree_ref()
-        node = self._follow(self._tree_ref)
-        while node is not None:
-            if key < node.key:
-                node = self._follow(node.left_ref)
-            elif node.key < key:
-                node = self._follow(node.right_ref)
-            else:
-                return node.value
-        raise KeyError
-
-    def set(self, key, value):
-        if self._storage.lock():
-            self._refresh_tree_ref()
-        self._tree_ref = self._insert(
-            self._follow(self._tree_ref), key, value)
-
-    def _insert(self, node, key, value):
-        if node is None:
-            new_node = BinaryNode(NodeRef(), key, value, NodeRef(), 1)
-        elif key < node.key:
-            new_node = BinaryNode.from_node(
-                node,
-                left_ref=self._insert(
-                    self._follow(node.left_ref), key, value))
-        elif node.key < key:
-            new_node = BinaryNode.from_node(
-                node,
-                right_ref=self._insert(
-                    self._follow(node.right_ref), key, value))
-        else:
-            new_node = BinaryNode.from_node(node, value=value)
-        return NodeRef(node=new_node)
-
-    def pop(self, key):
-        if self._storage.lock():
-            self._refresh_tree_ref()
-        self._tree_ref = self._delete(
-            self._follow(self._tree_ref), key)
-
-    def _delete(self, node, key):
-        if node is None:
-            raise KeyError
-        elif key < node.key:
-            new_node = BinaryNode.from_node(
-                node,
-                left_ref=self._delete(
-                    self._follow(node.left_ref), key))
-        elif node.key < key:
-            new_node = BinaryNode.from_node(
-                node,
-                right_ref=self._delete(
-                    self._follow(node.right_ref), key))
-        else:
-            left = self._follow(node.left_ref)
-            right = self._follow(node.right_ref)
-            if left and right:
-                replacement = self._find_max(left)
-                left_ref = self._delete(
-                    self._follow(node.left_ref), replacement.key)
-                new_node = BinaryNode(
-                    left_ref,
-                    replacement.key,
-                    replacement.value,
-                    node.right_ref,
-                    left_ref.length + node.right_ref.length + 1,
-                )
-            elif left:
-                return node.left_ref
-            else:
-                return node.right_ref
-        return NodeRef(node=new_node)
-
-    def _find_max(self, node):
-        while True:
-            next_node = self._follow(node.right_ref)
-            if next_node is None:
-                return node
-            node = next_node
-
-    def _follow(self, ref):
-        return ref.get(self._storage)
-
-    def __len__(self):
-        if not self._storage.locked:
-            self._refresh_tree_ref()
-        root = self._follow(self._tree_ref)
-        if root:
-            return root.length
-        else:
-            return 0
+from dbdb.tree import NodeRef, Tree
 
 
 class BinaryNode(object):
@@ -166,3 +58,75 @@ class BinaryNode(object):
 
 class BinaryNodeRef(NodeRef):
     node_class = BinaryNode
+
+
+class BinaryTree(Tree):
+    node_ref_class = BinaryNodeRef
+
+    def _get(self, node, key):
+        while node is not None:
+            if key < node.key:
+                node = self._follow(node.left_ref)
+            elif node.key < key:
+                node = self._follow(node.right_ref)
+            else:
+                return node.value
+        raise KeyError
+
+    def _insert(self, node, key, value):
+        if node is None:
+            new_node = BinaryNode(
+                self.node_ref_class(), key, value, self.node_ref_class(), 1)
+        elif key < node.key:
+            new_node = BinaryNode.from_node(
+                node,
+                left_ref=self._insert(
+                    self._follow(node.left_ref), key, value))
+        elif node.key < key:
+            new_node = BinaryNode.from_node(
+                node,
+                right_ref=self._insert(
+                    self._follow(node.right_ref), key, value))
+        else:
+            new_node = BinaryNode.from_node(node, value=value)
+        return NodeRef(node=new_node)
+
+    def _delete(self, node, key):
+        if node is None:
+            raise KeyError
+        elif key < node.key:
+            new_node = BinaryNode.from_node(
+                node,
+                left_ref=self._delete(
+                    self._follow(node.left_ref), key))
+        elif node.key < key:
+            new_node = BinaryNode.from_node(
+                node,
+                right_ref=self._delete(
+                    self._follow(node.right_ref), key))
+        else:
+            left = self._follow(node.left_ref)
+            right = self._follow(node.right_ref)
+            if left and right:
+                replacement = self._find_max(left)
+                left_ref = self._delete(
+                    self._follow(node.left_ref), replacement.key)
+                new_node = BinaryNode(
+                    left_ref,
+                    replacement.key,
+                    replacement.value,
+                    node.right_ref,
+                    left_ref.length + node.right_ref.length + 1,
+                )
+            elif left:
+                return node.left_ref
+            else:
+                return node.right_ref
+        return NodeRef(node=new_node)
+
+    def _find_max(self, node):
+        while True:
+            next_node = self._follow(node.right_ref)
+            if next_node is None:
+                return node
+            node = next_node
---
+++ data-store/dbdb/tree.py
@@ -0,0 +1,75 @@
+class Tree(object):
+    node_ref_class = None
+
+    def __init__(self, storage):
+        self._storage = storage
+        self._refresh_tree_ref()
+
+    def commit(self):
+        self._tree_ref.store(self._storage)
+        self._storage.commit_root_address(self._tree_ref.address)
+
+    def _refresh_tree_ref(self):
+        self._tree_ref = self.node_ref_class(
+            address=self._storage.get_root_address())
+
+    def get(self, key):
+        if not self._storage.locked:
+            self._refresh_tree_ref()
+        return self._get(self._follow(self._tree_ref), key)
+
+    def set(self, key, value):
+        if self._storage.lock():
+            self._refresh_tree_ref()
+        self._tree_ref = self._insert(
+            self._follow(self._tree_ref), key, value)
+
+    def pop(self, key):
+        if self._storage.lock():
+            self._refresh_tree_ref()
+        self._tree_ref = self._delete(
+            self._follow(self._tree_ref), key)
+
+    def _follow(self, ref):
+        return ref.get(self._storage)
+
+    def __len__(self):
+        if not self._storage.locked:
+            self._refresh_tree_ref()
+        root = self._follow(self._tree_ref)
+        if root:
+            return root.length
+        else:
+            return 0
+
+
+class NodeRef(object):
+    node_class = None
+
+    def __init__(self, node=None, address=0):
+        self._node = node
+        self._address = address
+
+    @property
+    def address(self):
+        return self._address
+
+    @property
+    def length(self):
+        if self._node is None and self._address:
+            raise RuntimeError('Asking for NodeRef length of unloaded node')
+        if self._node:
+            return self._node.length
+        else:
+            return 0
+
+    def get(self, storage):
+        if self._node is None and self._address:
+            self._node = self.node_class.from_string(
+                storage.read(self._address))
+        return self._node
+
+    def store(self, storage):
+        if self._node is not None and not self._address:
+            self._node.store_refs(storage)
+            self._address = storage.write(self._node.to_string())
