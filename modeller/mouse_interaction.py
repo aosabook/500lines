b1160ce88a6058f7490a03f7c26f93467d127eca
Erick Dransch <ekgdrans@uwaterloo.ca>
2014-01-05 18:54:59 -0500

    add mouse controls to move the camera

---
+++ modeller/mouse_interaction.py
@@ -0,0 +1,78 @@
+import sys
+import math
+
+from OpenGLContext import quaternion
+from OpenGLContext.move import trackball
+from OpenGLContext.arrays import *
+
+try:
+  from OpenGL.GLUT import *
+  from OpenGL.GL import *
+  from OpenGL.GLU import *
+except:
+  print ''' Error: PyOpenGL not installed properly !!'''
+  sys.exit(  )
+
+
+class MouseInteraction(object):
+
+    def __init__(self):
+        self.pressed = None
+        self.rotation = quaternion.fromEuler()
+        self.camera_loc = array( [0, 0, -15, 0], 'd')
+        self.trackball = None
+        self.mouse_loc = None
+        pass
+
+
+    def MouseButton(self, button, mode, x, y):
+        xSize, ySize = glutGet( GLUT_WINDOW_WIDTH ), glutGet( GLUT_WINDOW_HEIGHT )
+        self.mouse_loc = (x, y)
+        if mode == GLUT_DOWN:
+            print "DOWN" + str(button)
+            self.pressed = button
+            if button == GLUT_RIGHT_BUTTON:
+                self.trackball = trackball.Trackball(self.camera_loc, self.rotation, (0,0,0), x, y, xSize, ySize)
+            elif button == 3: # scroll up
+                self.translate(0, 0, 0.5)
+                glutPostRedisplay()
+            elif button == 4: # scroll up
+                self.translate(0, 0, -0.5)
+                glutPostRedisplay()
+
+
+        else:
+            print "UP" + str(button)
+            self.trackball = None
+            self.pressed = None
+        glutPostRedisplay()
+
+    def translate(self, x, y, z):
+       print x, y, z
+       self.camera_loc[0] += x
+       self.camera_loc[1] += y
+       self.camera_loc[2] += z
+
+
+    def MouseMove(self, x, y):
+        xSize, ySize = glutGet( GLUT_WINDOW_WIDTH ), glutGet( GLUT_WINDOW_HEIGHT )
+        y = ySize - y
+        if self.pressed is not None:
+            if self.pressed == GLUT_RIGHT_BUTTON and self.trackball is not None:
+                _, self.rotation = self.trackball.update(x, y)
+            elif self.pressed == GLUT_LEFT_BUTTON:
+                dx = x - self.mouse_loc[0]
+                dy = y - self.mouse_loc[1]
+                self.translate(dx/60.0, dy/60.0, 0)
+            elif self.pressed == GLUT_MIDDLE_BUTTON:
+                dz = y - self.mouse_loc[1]
+                self.translate(0, 0, dz/40.0)
+            else:
+                pass
+        self.mouse_loc = (x, y)
+        glutPostRedisplay()
+
+
+    def register(self):
+        glutMouseFunc(self.MouseButton)
+        glutMotionFunc(self.MouseMove)
--- modeller/viewer.py
+++ modeller/viewer.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 from OpenGLContext import testingcontext
 from OpenGLContext import quaternion
+from OpenGLContext import contextdefinition
 from OpenGLContext.arrays import *
 from OpenGLContext.scenegraph.basenodes import Sphere
 BaseContext = testingcontext.getInteractive()
@@ -8,24 +9,27 @@ BaseContext = testingcontext.getInteractive()
 from OpenGL.GL import *
 from OpenGL.GL import shaders
 from OpenGL.GLU import *
+from OpenGL.GLUT import *
 from OpenGL.arrays import vbo
 OBJ_PLANE = 1
+
+from mouse_interaction import MouseInteraction
 
 class TestContext(BaseContext):
     def OnInit(self):
         self.MakePlane()
-        self.rotation = quaternion.fromEuler()
+        self.mouse_interaction = MouseInteraction()
+        self.mouse_interaction.register()
         glPushMatrix()
         glLoadIdentity()
-        glTranslatef(0, 0, -20)
-        self.translation = glGetFloatv(GL_MODELVIEW_MATRIX)
+        glTranslatef(0, 0, -10)
+
         glPopMatrix()
 
     def Lights(self, mode = None):
         """ called by default rendering process iff there is no scenegraph present """
         glEnable(GL_LIGHTING)
         glEnable(GL_LIGHT0)
-        print "LIGHTING"
 
     def MakePlane(self):
         glNewList(OBJ_PLANE, GL_COMPILE)
@@ -90,9 +94,22 @@ class TestContext(BaseContext):
     def Render(self, mode = None):
         BaseContext.Render(self, mode)
 
+        xSize, ySize = glutGet( GLUT_WINDOW_WIDTH ), glutGet( GLUT_WINDOW_HEIGHT )
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glViewport(0, 0, xSize, ySize);
+        gluPerspective(40.0, 1.0, 0.1, 1000.0);
+        loc = self.mouse_interaction.camera_loc
+
+        glTranslated(loc[0], loc[1], loc[2]);
+        # use glMultMatrix
+
+
+        glMatrixMode(GL_MODELVIEW);
         glEnable(GL_LIGHTING)
         glEnable(GL_LIGHT0)
-        glLightfv(GL_LIGHT0, GL_POSITION, GLfloat_4(0, 0, -15, 1))
+        glLightfv(GL_LIGHT0, GL_POSITION, GLfloat_4(0, 0, -10, 1))
         glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, GLfloat_3(0, 0, 1))
 
         glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
@@ -100,15 +117,12 @@ class TestContext(BaseContext):
 
         glClearColor(0.4, 0.4, 0.4, 0.0);
         glClear(GL_COLOR_BUFFER_BIT)
-        glMatrixMode(GL_MODELVIEW)
         glPushMatrix()
         glLoadIdentity()
-        glMultMatrixf(self.translation)
-        glMultMatrixf(self.rotation.matrix(inverse=True))
+        glMultMatrixf(self.mouse_interaction.rotation.matrix(inverse=False))
 
         glCallList(OBJ_PLANE)
-#        self.MakePlane()
         glPopMatrix()
 
 if __name__=="__main__":
-    TestContext.ContextMainLoop()
+    TestContext.ContextMainLoop( definition = contextdefinition.ContextDefinition(size = (500,500),))
