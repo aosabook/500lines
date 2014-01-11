ec33e01263aac31dcf58d58c013d0ca9d07d306d
Doc Ritezel and Sarah Mei <pair+doc+sarah@ministryofvelocity.com>
2014-01-11 15:49:36 -0800

    Add before and after hooks

--- catechism/lib/catechism/describe_block.rb
+++ catechism/lib/catechism/describe_block.rb
@@ -1,7 +1,26 @@
 require "catechism/it_block"
 
 class Catechism::DescribeBlock < Struct.new(:description)
+  def before(&block)
+    before_blocks << block if block_given?
+  end
+
+  def after(&block)
+    after_blocks << block if block_given?
+  end
+
   def it(description, &block)
-    Catechism::ItBlock.new(description, self).instance_eval(&block) if block_given?
+    it_block = Catechism::ItBlock.new(description, self)
+    before_blocks.each { |before_block| it_block.instance_eval(&before_block) }
+    it_block.instance_eval(&block) if block_given?
+    after_blocks.each { |after_block| it_block.instance_eval(&after_block) }
+  end
+
+  def before_blocks
+    @before_blocks ||= []
+  end
+
+  def after_blocks
+    @after_blocks ||= []
   end
 end
