56f26eee53b23624fbdbfb453ae2b1115cfa4078
Laurie MacDougall Sookraj <lmds28@gmail.com>
2014-02-12 22:28:42 -0500

    added whiskers templates to make basic wiki less garbagey.

--- wiki/part1/js/server.js
+++ wiki/part1/js/server.js
@@ -1,8 +1,11 @@
-const express = require('express'),
+var express = require('express'),
       fs = require('fs'),
+      whiskers = require('whiskers'),
       app = express();
 
-app.use(express.bodyParser());
+app.use(express.bodyParser()); //for post parameter parsing
+app.engine('.html', whiskers.__express);
+app.set('views', __dirname+'/../views');
 
 getWikiContents = function(page, callback){
   return fs.readFile("../files/"+page, function(err, data){
@@ -12,7 +15,7 @@ getWikiContents = function(page, callback){
 
 saveWikiContents = function(page, contents, callback){
   fs.writeFile('../files/'+page, contents, function(err){
-    if(err) throw err;
+    if(err) return callback(false);
     callback(true);
   });
 }
@@ -22,71 +25,41 @@ app.get('/wiki/', function(request, response){
   //get listing of available pages from disk
   //for each page, render a link
   fs.readdir('../files/', function(err, files){
-    if(err) response.send(500, err);
-    var html = "<html><head><title>wiki</title></head><body><h1>Existing pages</h1>";
-    for(var i=0; i<files.length; i++){
-      html += '<a href="/wiki/'+files[i]+'" >' + files[i] + "</a><br/>";
-    }
-    html += "</body></html>";
-    response.send(200, html);
+    if(err) return response.send(500, err);
+    response.render('list.html', {pages: files});
   });
-
 });
 
 app.get('/wiki/:page', function(request, response){
   var page = request.params.page;
-  console.log('got view request for page ' + page);
-
-  //read page from disk if it exists
+  //read page if it exists
   var content = getWikiContents(page, function(content){
     if(!content) content = '';
-    //add edit link
     var sep = /\/$/.test(request.path) ? '': '/';
     var editLink = request.path + sep + "edit";
-    content += '<br/><a href="'+editLink+'">Edit</a>';
     //show contents of page
-    response.send(200, '<html><head><title>'+page+'</title></head>'
-                  +'<body><h1>'+page+'</h1>'+content+'</body></html>');
+    response.render('view.html', {title: page, content: content, editLink: editLink} );
   });
-
 });
 
 app.get('/wiki/:page/edit', function(request, response){
-  console.log('got edit request');
-  //show editor
   var page = request.params.page;
   var url = request.path;
   var content = getWikiContents(page, function(content){
     if(!content) content = ''; //no contents yet
-    var editor = '<form action="'+url+'" method="post">'
-    +'<textarea name="content" style="width:80%;height:80%">'+content+'</textarea>'
-    +'<button type="submit">Save</button></form>';
-    response.send(200, '<html><head><title>'+page+'</title></head>'
-                +'<body><h1>'+page+'</h1>'+editor+'</body></html>');
+    response.render('edit.html', {title: page, content: content, url: url} );
   });
-
-
 });
 
 app.post('/wiki/:page/edit', function(request, response){
-  console.log('got new edit');
-  //save edits
   var page = request.params.page;
-  var content = request.body.content;
+  var content = request.body.content; //TODO: sanitize input
   saveWikiContents(page, content, function(success){
-    //return to view
-    if(success) response.redirect('/wiki/'+page);
+    if(success) response.redirect('/wiki/'+page); //return to view
     else{ //display contents in editor
-      var editor = '<form action="'+request.path+'" method="post">'
-      +'<textarea name="content" style="width:80%;height:80%">'+content+'</textarea>'
-      +'<button type="submit">Save</button></form>';
-      editor += "<p>Unable to save contents</p>";
-      response.send(200, '<html><head><title>'+page+'</title></head>'
-                  +'<body><h1>'+page+'</h1>'+editor+'</body></html>');
+      response.render('edit.html', {title: page, content: content, url: request.path, error:'Unable to save contents'} );
     }
   });
-
-
 });
 
 app.listen(8080, function(){
