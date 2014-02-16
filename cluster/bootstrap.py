dd0a2dbeb16e24896f1f621ae75b16b434727c4d
Dustin J. Mitchell <dustin@mozilla.com>
2014-02-16 14:43:19 -0500

    stop sending JOIN when bootstrapping is complete

--- cluster/bootstrap.py
+++ cluster/bootstrap.py
@@ -8,6 +8,7 @@ class Bootstrap(Component):
     def __init__(self, member, peers, bootstrapped_cb):
         super(Bootstrap, self).__init__(member)
         self.peers = peers
+        self.timer = None
         self.bootstrapped_cb = bootstrapped_cb
 
     def start(self):
@@ -17,9 +18,11 @@ class Bootstrap(Component):
         "Try to join the cluster"
         self.peers = self.peers[1:] + self.peers[:1] # rotate through peers
         self.send([self.peers[0]], 'JOIN', requester=self.address)
-        self.set_timer(protocol.JOIN_RETRANSMIT, self.join)
+        self.timer = self.set_timer(protocol.JOIN_RETRANSMIT, self.join)
 
     def do_WELCOME(self, state, slot_num, decisions, viewid, peers):
         self.bootstrapped_cb(state, slot_num, decisions, viewid, peers)
         self.event('view_change', viewchange=ViewChange(viewid, peers))  # TODO: pass viewid, peers separately
+        if self.timer:
+            self.cancel_timer(self.timer)
         self.stop()
