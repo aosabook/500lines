ec54c70e47e3b2229e68f6ccf3177b49d21eb42f
Yoav Rubin <yoavrubin@hotmail.com>
2014-02-17 22:58:06 +0200

    adding dbs and db-connection apis. For now creation and consumption

---
+++ functionalDB/src/core/manage.clj
@@ -0,0 +1,15 @@
+(ns tmpTst (:use core.fdb))
+
+(def all-dbs (atom {}))
+
+(defn _get-db [dbs db-name]
+  (if (db-name dbs ) dbs (assoc dbs db-name (make-db))))
+
+(defn get-db-conn [db-name]
+  ((keyword db-name) (swap! all-dbs _get-db (keyword db-name))))
+
+(defn db-from-conn [conn] @conn)
+
+(get-db-conn "aa")
+(get-db-conn "ab")
+@all-dbs
