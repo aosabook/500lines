9b3eba9382f37863bd974b9c67430f3358817d65
Darius Bacon <darius@wry.me>
2014-02-19 22:52:25 -0800

    add assembler and port the extensions back from the direct-composition branch

---
+++ bytecode-compiler/assembler.py
@@ -0,0 +1,67 @@
+"""
+Produce bytecode bytestrings from a hierarchical symbolic form with
+local labels for addresses.
+"""
+
+import dis
+
+def take_argument(opcode):
+    code0 = encode(opcode, 0)
+    if opcode in dis.hasjrel:
+        return lambda label: (code0, [(label, lambda addr: addr)])
+    elif opcode in dis.hasjabs:
+        return lambda label: (code0, [(label, lambda addr: 0)])
+    else:
+        return lambda arg: (encode(opcode, arg), [])
+
+def encode(opcode, arg): return [opcode, arg % 256, arg // 256]
+
+class Opcodes: pass
+op = Opcodes()
+for name, opcode in dis.opmap.items():
+    setattr(op, name,
+            ([opcode], []) if opcode < dis.HAVE_ARGUMENT else take_argument(opcode))
+
+def assemble(assembly):
+    code = []
+
+    def flatten(assembly, refs):
+        if isinstance(assembly, tuple):
+            assert len(assembly) == 2, assembly
+            my_code, my_linking = assembly
+            refs.extend((len(code) + 1, label, fixup)
+                        for label, fixup in my_linking)
+            code.extend(my_code)
+        elif isinstance(assembly, list):
+            for subassembly in assembly:
+                flatten(subassembly, refs)
+        elif isinstance(assembly, dict):
+            my_refs = []
+            my_addresses = {}
+            for label, subassembly in sorted(assembly.items()):
+                my_addresses[label] = len(code)
+                flatten(subassembly, my_refs)
+            for address, label, fixup in my_refs:
+                target = my_addresses[label] - fixup(address+2)
+                code[address+0] = target % 256
+                code[address+1] = target // 256
+        else:
+            assert False, "Yo: {}".format(assembly)
+
+    refs = []
+    flatten(assembly, refs)
+    assert not refs
+    return bytes(tuple(code))
+
+
+if __name__ == '__main__':
+    example = {0: [op.LOAD_CONST(0), op.POP_JUMP_IF_FALSE(1),
+                   op.LOAD_CONST(1), op.JUMP_FORWARD(2)],
+               1: [op.LOAD_CONST(2)],
+               2: []}
+## op.LOAD_CONST(0)
+#. ([100, 0, 0], [])
+## op.POP_JUMP_IF_FALSE(1)
+#. ([114, 0, 0], [(1, <function <lambda> at 0xffe79764>)])
+    for pc, byte in enumerate(assemble(example)):
+        print(pc, byte)
--- bytecode-compiler/bytecomp.py
+++ bytecode-compiler/bytecomp.py
@@ -1,19 +1,13 @@
 """
-Byte-compile trivial programs like "print(2+3)".
+Byte-compile trivial programs with if, while, globals, calls, and
+a bit more.
 """
 
-import ast
-from collections import defaultdict
-from dis import dis, opmap 
-from types import CodeType, FunctionType
+import ast, collections, dis, types
+from assembler import op, assemble
 
-class Opcodes: pass
-opc = Opcodes()
-opc.__dict__.update(opmap)
-
-def bytecomp(node, f_globals):
-    code = CodeGen().compile(node)
-    return FunctionType(code, f_globals)
+def bytecomp(t, f_globals):
+    return types.FunctionType(CodeGen().compile(t), f_globals)
 
 class CodeGen(ast.NodeVisitor):
 
@@ -22,80 +16,123 @@ class CodeGen(ast.NodeVisitor):
         self.names     = make_table()
         self.varnames  = make_table()
 
-    def compile(self, node):
-        bytecode = self.visit(node)
-        bytecode = self.sequence([bytecode,
-                                  ((opc.LOAD_CONST,) + encode(self.constants[None])
-                                   + (opc.RETURN_VALUE,))])
+    def compile_body(self, body):
+        t = body[0]
+        if not (isinstance(t, ast.Expr) and isinstance(t.value, ast.Str)):
+            self.constants[None] # The doc comment starts the constant table.
+        return self.compile(body)
+
+    def compile(self, t):
+        bytecode = [self.of(t), self.load_const(None), op.RETURN_VALUE]
         argcount = 0
         kwonlyargcount = 0
         nlocals = 0
         stacksize = 10          # XXX
-        flags = 67
+        flags = 64  # XXX I don't understand the flags
         filename = '<stdin>'
         name = 'the_name'
         firstlineno = 1
         lnotab = b''
-        return CodeType(argcount, kwonlyargcount, nlocals, stacksize, flags,
-                        bytes(bytecode),
-                        collect(self.constants),
-                        collect(self.names),
-                        collect(self.varnames),
-                        filename, name, firstlineno, lnotab,
-                        freevars=(), cellvars=())
+        return types.CodeType(argcount, kwonlyargcount, nlocals, stacksize, flags,
+                              assemble(bytecode),
+                              collect(self.constants),
+                              collect(self.names),
+                              collect(self.varnames),
+                              filename, name, firstlineno, lnotab,
+                              freevars=(), cellvars=())
 
-    def sequence(self, codes):  # TODO: use join method?
-        result = ()
-        for code in codes:
-            if result: result += (opc.POP_TOP,)
-            result += code
-        return result
+    def of(self, t):
+        return list(map(self.of, t)) if isinstance(t, list) else self.visit(t)
 
-    def visits(self, nodes):
-        return sum(map(self.visit, nodes), ())
+    def load_const(self, constant):
+        return op.LOAD_CONST(self.constants[constant])
 
-    def visit_Num(self, node):
-        return (opc.LOAD_CONST,) + encode(self.constants[node.n])
+    def store(self, name):
+        return op.STORE_NAME(self.names[name])
 
-    def visit_Name(self, node):
-        return (opc.LOAD_GLOBAL,) + encode(self.names[node.id])  # XXX LOAD_NAME in general
+    def visit_Module(self, t):
+        return self.of(t.body)
 
-    def visit_BinOp(self, node):
-        binops = {ast.Add: opc.BINARY_ADD}
-        return self.visit(node.left) + self.visit(node.right) + (binops[type(node.op)],)
+    def visit_FunctionDef(self, t):
+        assert not t.args.args
+        assert not t.decorator_list
+        code = CodeGen().compile_body(t.body)
+        return [self.load_const(code), op.MAKE_FUNCTION(0), self.store(t.name)]
 
-    def visit_Call(self, node):
-        return (self.visit(node.func) + self.visits(node.args) + (opc.CALL_FUNCTION,) + encode(len(node.args)))
+    def visit_If(self, t):
+        return {0: [self.of(t.test), op.POP_JUMP_IF_FALSE(1),
+                    self.of(t.body), op.JUMP_FORWARD(2)],
+                1: [self.of(t.orelse)],
+                2: []}
 
-    def visit_Expr(self, node):
-        return self.visit(node.value)
+    def visit_While(self, t):
+        return {0: [op.SETUP_LOOP(2)],
+                1: [self.of(t.test), op.POP_JUMP_IF_FALSE(2),
+                    self.of(t.body), op.JUMP_ABSOLUTE(1)],
+                2: [op.POP_BLOCK]}
 
-    def visit_Assign(self, node):
-        assert 1 == len(node.targets)
-        assert isinstance(node.targets[0], ast.Name)
-        return self.visit(node.value) + (opc.DUP_TOP, opc.STORE_GLOBAL,) + encode(self.names[node.targets[0].id])
+    def visit_Expr(self, t):
+        return [self.of(t.value), op.POP_TOP]
 
-    def visit_Module(self, node):
-        return self.sequence(map(self.visit, node.body))
+    def visit_Assign(self, t):
+        assert 1 == len(t.targets) and isinstance(t.targets[0], ast.Name)
+        return [self.of(t.value), op.DUP_TOP, self.store(t.targets[0].id)]
+
+    def visit_Call(self, t):
+        return [self.of(t.func), self.of(t.args), op.CALL_FUNCTION(len(t.args))]
+
+    def visit_BinOp(self, t):
+        return [self.of(t.left), self.of(t.right), self.ops2[type(t.op)]]
+    ops2 = {ast.Add:    op.BINARY_ADD,      ast.Sub:      op.BINARY_SUBTRACT,
+            ast.Mult:   op.BINARY_MULTIPLY, ast.Div:      op.BINARY_TRUE_DIVIDE,
+            ast.Mod:    op.BINARY_MODULO,   ast.Pow:      op.BINARY_POWER,
+            ast.LShift: op.BINARY_LSHIFT,   ast.RShift:   op.BINARY_RSHIFT,
+            ast.BitOr:  op.BINARY_OR,       ast.BitXor:   op.BINARY_XOR,
+            ast.BitAnd: op.BINARY_AND,      ast.FloorDiv: op.BINARY_FLOOR_DIVIDE}
+
+    def visit_Num(self, t):
+        return self.load_const(t.n)
+
+    def visit_Str(self, t):
+        return self.load_const(t.s)
+
+    def visit_Name(self, t):
+        return op.LOAD_NAME(self.names[t.id])
 
 def make_table():
-    table = defaultdict(lambda: len(table))
+    table = collections.defaultdict(lambda: len(table))
     return table
 
 def collect(table):
     return tuple(sorted(table, key=table.get))
 
-def encode(u):
-    return (u % 256, u // 256)
-
 
 if __name__ == '__main__':
+
+    def diss(code):
+        dis.dis(code)
+        for c in code.co_consts:
+            if isinstance(c, types.CodeType):
+                print()
+                print('------', c, '------')
+                diss(c)
+
     eg_ast = ast.parse("""
 a = 2+3
-print(a, 137)
+def f():
+    "doc comment"
+    while a:
+        if a - 1:
+            print(a, 137)
+        a = a - 1
+f()
 print(pow(2, 16))
-    """)
-    print(ast.dump(eg_ast))
+""")
+    try:
+        import astpp
+    except ImportError:
+        astpp = ast
+    print(astpp.dump(eg_ast))
     f = bytecomp(eg_ast, globals())
-    dis(f)
+    diss(f.__code__)
     f()   # It's alive!
