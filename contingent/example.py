8cc2d98a6fc11641a4c3b2bb0af0203ff6df9417
Brandon Rhodes <brandon@rhodesmill.org>
2014-02-27 21:32:00 -0500

    Teach example posts to display previous, next post

--- contingent/example.py
+++ contingent/example.py
@@ -1,6 +1,6 @@
+"""Rough experiment from which to derive the design of `contingent`."""
 
-
-
+from operator import itemgetter
 
 post1 = {
     'title': 'A',
@@ -27,16 +27,36 @@ new_post = {
     }
 
 def main():
-    print render(post1)
+    all_posts = [post1, post2, post3]
+    for post in all_posts:
+        print '-' * 8
+        print render(post, all_posts)
 
-def render(post):
+def prev_post(post, all_posts):
+    s = sorted(all_posts, key=itemgetter('date'))
+    i = s.index(post)
+    return s[i-1] if i > 0 else None
+
+def next_post(post, all_posts):
+    s = sorted(all_posts, key=itemgetter('date'))
+    i = s.index(post)
+    return s[i+1] if i < len(s) - 1 else None
+
+def render(post, all_posts):
+    pp = prev_post(post, all_posts)
+    np = next_post(post, all_posts)
+
     kw = dict(post)
+    kw['prev_title'] = pp['title'] if (pp is not None) else '-'
+    kw['next_title'] = np['title'] if (np is not None) else '-'
     return template.format(**kw)
 
-template = """
-{title}
-{date}
+template = """\
+Previous story: {prev_title}
+Next story: {next_title}
 
+"{title}"
+{date}
 {content}
 """
 
