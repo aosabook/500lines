1eea04d7368f5e1fd94f18316cd786ac8bf73b9a
Allison Kaptur <allison.kaptur@gmail.com>
2014-03-03 17:31:13 -0500

    more simplification of entry

--- interpreter/byterun/__main__.py
+++ interpreter/byterun/__main__.py
@@ -5,25 +5,22 @@ from pyvm2 import VirtualMachine
 
 def run_python_file(filename):
     """Run a python file as if it were the main program on the command line.
-    `filename` is the path to the file to execute, it need not be a .py file.
+    `filename` is the path to the file to execute.
     """
     old_main_mod = sys.modules['__main__']
     main_mod = imp.new_module('__main__') # Create a module to serve as __main__
     sys.modules['__main__'] = main_mod
     main_mod.__builtins__ = sys.modules['builtins']
 
-    try:
-        with open(filename, 'rU') as f:
-            source = f.read()
+    with open(filename, 'rU') as f:
+        source = f.read()
 
-        if not source or source[-1] != '\n':
-            source += '\n' # `compile` still needs the last line to be clean,
-        code = compile(source, filename, "exec")
+    if not source or source[-1] != '\n':
+        source += '\n' # `compile` needs the last line to be clean
+    code = compile(source, filename, "exec")
 
-        vm = VirtualMachine()
-        vm.run_code(code, f_globals=main_mod.__dict__)
-    finally:
-        sys.modules['__main__'] = old_main_mod # Restore the old __main__
+    vm = VirtualMachine()
+    vm.run_code(code, f_globals=main_mod.__dict__)
 
 if __name__ == '__main__':
     run_python_file(sys.argv[1])
