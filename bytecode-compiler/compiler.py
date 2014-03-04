8f657e6687aef7bef2b95b1ecbd1d3be8c8758b1
Darius Bacon <darius@wry.me>
2014-03-03 16:23:25 -0800

    extract example code to silly.py

--- bytecode-compiler/compiler.py
+++ bytecode-compiler/compiler.py
@@ -42,38 +42,8 @@ def codepp(code):
             report(k, getattr(code, k))
 
 
-
 if __name__ == '__main__':
-
-    eg = """
-#import math
-print(['m', 'n'][0])
-(pow, len)
-{'a': 42, 'b': 55}
-{}
-None
-ga = 2+3
-def f(a):
-    "doc comment"
-    while a and True:
-        pass
-        if False or a != 1 or False:
-            print(a, 137)
-        a = a - 1
-    return pow(2, 16)
-    return
-print(f(ga))
-t = True
-while t:
-    break
-for i in range(3):
-    print(i)
-#print(-math.sqrt(2))
-raise Exception('hi')
-"""
-    if len(sys.argv) == 1:
-        main(eg)
-    elif len(sys.argv) >= 2:
+    if len(sys.argv) >= 2:
         del sys.argv[0]
         filename = sys.argv[0]
         main(open(filename).read(), filename)
