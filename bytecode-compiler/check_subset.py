4bb2f12ea3d355e7f3fad0a890ae1e11e3ad5d00
Darius Bacon <darius@wry.me>
2014-03-07 16:15:31 -0800

    checker closer to complete

--- bytecode-compiler/check_subset.py
+++ bytecode-compiler/check_subset.py
@@ -26,83 +26,70 @@ class Checker(ast.NodeVisitor):
         self(t.body)
 
     def visit_FunctionDef(self, t):
-#        self(t.args)
+        self.check_identifier(t.name)
+        self.check_arguments(t.args)
         self(t.body)
         assert not t.decorator_list
         assert not t.returns
 
     def visit_ClassDef(self, t):
-        assert not t.decorator_list
+        self.check_identifier(t.name)
         self(t.bases)
-
-    def visit_Import(self, t):
-        self(t.names)
+        assert not t.keywords
+        assert not t.starargs
+        assert not t.kwargs
+        assert not t.decorator_list
+        self(t.body)
 
     def visit_Return(self, t):
         if t.value is not None:
             self(t.value)
+
+    def visit_Assign(self, t):
+        self(t.targets)
+        self(t.value)
+
+    def visit_For(self, t):
+        self(t.target)
+        self(t.iter)
+        self(t.body)
+        assert not t.orelse
+
+    def visit_While(self, t):
+        self(t.test)
+        self(t.body)
+        assert not t.orelse
 
     def visit_If(self, t):
         self(t.test)
         self(t.body)
         self(t.orelse)
 
-    visit_IfExp = visit_If
-
-    def visit_While(self, t):
-        self(t.test)
-        self(t.body)
-
-    def visit_For(self, t):
-        self(t.iter)
-        t.target.id
-        self(t.body)
-
     def visit_Raise(self, t):
         self(t.exc)
+        assert not t.cause
+
+    def visit_Import(self, t):
+        self(t.names)
 
     def visit_Expr(self, t):
         self(t.value)
 
-    def visit_Assign(self, t):
-        self(t.value)
-        self(t.targets)
+    def visit_Pass(self, t):
+        pass
 
-    def visit_Call(self, t):
-        self(t.func)
-        self(t.args)
+    def visit_Break(self, t):
+        pass # XXX
 
-    def visit_sequence(self, t):
-        if   isinstance(t.ctx, ast.Load):
-            self(t.elts)
-        elif isinstance(t.ctx, ast.Store):
-            # XXX make sure there are no stars in elts
-            self(t.elts)
-        else:
-            assert False
-
-    visit_List = visit_sequence
-    visit_Tuple = visit_sequence
-
-    def visit_Set(self, t):
-        self(t.elts)
-
-    def visit_Dict(self, t):
-        assert len(t.keys) <= 255
-        for k, v in zip(t.keys, t.values):
-            self(v)
-            self(k)
-
-    def visit_UnaryOp(self, t):
-        self(t.operand)
-        assert type(t.op) in self.ops1
-    ops1 = {ast.UAdd,  ast.Invert,
-            ast.USub,  ast.Not}
+    def visit_BoolOp(self, t):
+        assert type(t.op) in self.ops_bool
+        self(t.values)
+    ops_bool = {ast.And, ast.Or}
 
     def visit_BinOp(self, t):
+        assert type(t.op) in self.ops2
         self(t.left)
         self(t.right)
-        assert type(t.op) in self.ops2
     ops2 = {ast.Pow,       ast.Add,
             ast.LShift,    ast.Sub,
             ast.RShift,    ast.Mult,
@@ -110,26 +97,38 @@ class Checker(ast.NodeVisitor):
             ast.BitAnd,    ast.Div,
             ast.BitXor,    ast.FloorDiv}
 
+    def visit_UnaryOp(self, t):
+        assert type(t.op) in self.ops1
+        self(t.operand)
+    ops1 = {ast.UAdd,  ast.Invert,
+            ast.USub,  ast.Not}
+
+    visit_IfExp = visit_If
+
+    def visit_Dict(self, t):
+        for k, v in zip(t.keys, t.values):
+            self(v)
+            self(k)
+
+    def visit_Set(self, t):
+        self(t.elts)
+
     def visit_Compare(self, t):
-        assert 1 == len(t.ops)
         self(t.left)
-        self(t.comparators[0])
+        assert 1 == len(t.ops)
         assert type(t.ops[0]) in self.ops_cmp
+        assert len(t.ops) == len(t.comparators)
+        self(t.comparators[0])
     ops_cmp = {ast.Eq,  ast.NotEq,  ast.Is,  ast.IsNot,
                ast.Lt,  ast.LtE,    ast.In,  ast.NotIn,
                ast.Gt,  ast.GtE}
 
-    def visit_BoolOp(self, t):
-        self(t.values)
-        assert type(t.op) in self.ops_bool
-    ops_bool = {ast.And,
-                ast.Or}
-
-    def visit_Pass(self, t):
-        pass
-
-    def visit_Break(self, t):
-        pass # XXX
+    def visit_Call(self, t):
+        self(t.func)
+        self(t.args)
+        assert not hasattr(t, 'keywords') or not t.keywords
+        assert not hasattr(t, 'starargs') or not t.starargs
+        assert not hasattr(t, 'kwargs') or not t.kwargs
 
     def visit_Num(self, t):
         pass
@@ -141,19 +140,17 @@ class Checker(ast.NodeVisitor):
 
     def visit_Attribute(self, t):
         self(t.value)
-        if   isinstance(t.ctx, ast.Load):
-            pass
-        elif isinstance(t.ctx, ast.Store):
-            pass
-        else:
-            assert False
+        self.check_identifier(t.attr)
+        if   isinstance(t.ctx, ast.Load):  pass
+        elif isinstance(t.ctx, ast.Store): pass
+        else: assert False
 
     def visit_Subscript(self, t):
+        self(t.value)
         if isinstance(t.slice, ast.Index):
             if   isinstance(t.ctx, ast.Load):  pass
             elif isinstance(t.ctx, ast.Store): pass
             else: assert False
-            self(t.value)
             self(t.slice.value)
         else:
             assert False
@@ -162,6 +159,34 @@ class Checker(ast.NodeVisitor):
         pass
 
     def visit_Name(self, t):
+        self.check_identifier(t.id)
         if   isinstance(t.ctx, ast.Load):  pass
         elif isinstance(t.ctx, ast.Store): pass
         else: assert False
+
+    def visit_sequence(self, t):
+        self(t.elts)
+        # XXX make sure there are no stars in elts
+        if   isinstance(t.ctx, ast.Load):  pass
+        elif isinstance(t.ctx, ast.Store): pass
+        else: assert False
+
+    visit_List = visit_sequence
+    visit_Tuple = visit_sequence
+
+    def check_arguments(self, args):
+        for arg in args.args: self.check_arg(arg)
+        assert not args.vararg
+        assert not hasattr(args, 'varargannotation')
+        assert not args.kwonlyargs
+        assert not args.kwarg
+        assert not hasattr(args, 'kwargannotation')
+        assert not args.defaults
+        assert not args.kw_defaults
+
+    def check_arg(self, arg):
+        assert not arg.annotation
+
+    def check_identifier(self, name):
+        assert isinstance(name, str)
+
