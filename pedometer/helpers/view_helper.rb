0cdfdd8b3576afde316a5aec68165f593a90e5bb
Dessy Daskalov <dessy.daskalov@gmail.com>
2014-03-16 11:35:59 -0400

    Improve distance view helper method

--- pedometer/helpers/view_helper.rb
+++ pedometer/helpers/view_helper.rb
@@ -1,13 +1,15 @@
 class ViewHelper
 
+  # TODO: Can this be cleaner?
   def self.format_distance(distance_cm)
     distance_cm = distance_cm.round(2)
     if distance_cm >= 100000
       "#{(distance_cm/100000).round(2)} km"
     elsif distance_cm >= 100
-      "#{(distance_cm/100).round(2)} m"
+      distance = (distance_cm/100).round(2)
+      (distance == 1000) ? "1.0 km" : "#{distance} m"
     else
-      "#{(distance_cm).round(2)} cm"
+      (distance_cm == 100) ? "1.0 m" : "#{distance_cm} cm"
     end
   end
 
--- pedometer/test/unit/view_helper_test.rb
+++ pedometer/test/unit/view_helper_test.rb
@@ -4,18 +4,21 @@ require_relative '../../helpers/view_helper'
 class UIHelperTest < Test::Unit::TestCase
 
   def test_format_distance_cm
-    assert_equal '1.0 cm', ViewHelper.format_distance(1)
+    assert_equal '0.01 cm',  ViewHelper.format_distance(0.01)
+    assert_equal '1.0 cm',   ViewHelper.format_distance(1)
     assert_equal '99.99 cm', ViewHelper.format_distance(99.987)
+    assert_equal '1.0 m',    ViewHelper.format_distance(99.999)
   end
 
   def test_format_distance_m
-    assert_equal '1.0 m', ViewHelper.format_distance(99.999)
-    assert_equal '999.99 m', ViewHelper.format_distance(99999.9)
+    assert_equal '1.0 m',    ViewHelper.format_distance(100)
+    assert_equal '999.99 m', ViewHelper.format_distance(99998.99)
+    assert_equal '1.0 km',   ViewHelper.format_distance(99999.99)
   end
 
   def test_format_distance_km
-    assert_equal '1 Km', ViewHelper.format_distance(99999.9)
-    assert_equal '1 km', ViewHelper.format_distance(10000)
+    assert_equal '1.0 km',  ViewHelper.format_distance(100000)
+    assert_equal '1.99 km', ViewHelper.format_distance(199000)
   end
 
   def test_format_time
