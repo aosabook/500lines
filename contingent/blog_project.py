fb53fe57227ed699ea58781b2adfae390752679f
Brandon Rhodes <brandon@rhodesmill.org>
2014-03-19 22:45:49 -0400

    Add some docstrings, especially to builderlib.py

--- contingent/blog_project.py
+++ contingent/blog_project.py
@@ -6,12 +6,16 @@ from inspect import ismethod
 
 class Base:
     def __getattribute__(self, name):
+        """Instead of always computing attributes, ask our builder for them."""
+
         if name.startswith('_'):
             return object.__getattribute__(self, name)
         target = (self, name)
         return self._builder.get(target)
 
     def __setattr__(self, name, value):
+        """Perform a normal setattr, plus let the builder know about it."""
+
         if not name.startswith('_'):
             target = (self, name)
             self._builder.set(target, value)
--- contingent/builderlib.py
+++ contingent/builderlib.py
@@ -11,6 +11,19 @@ class Builder:
         self.target_stack = []
 
     def get(self, dependency):
+        """Return the value of a particular target.
+
+        This is the center of our entire operation!  The fact that
+        someone needs this dependency recomputed right now does two
+        crucial things.  First, it lets us build an edge from this
+        dependency to the target whose recompute is currently underway,
+        so that the graph will tell us to recompute that target when
+        this value changes in the future.  Second, it gives us the
+        choice to simply return the value of the dependency immediately
+        if it is already in our cache, which prevents our whole scheme
+        from being O(n^2).
+
+        """
         if self.target_stack:
             self.graph.add_edge(dependency, self.target_stack[-1])
         value = self.cache.get(dependency)
@@ -20,12 +33,24 @@ class Builder:
         return value
 
     def set(self, target, value):
+        """Inform the cache that a target is being updated.
+
+        This gives the cache the opportunity to compare the new value
+        against the old one to determine whether the tasks downstream
+        from the target should be added to the to-do list.
+
+        """
         self.graph.clear_dependencies_of(target)
-        if self.target_stack:
-            self.graph.add_edge(self.target_stack[-1], target)
         self.cache[target] = value
 
     def recompute(self, target):
+        """Recompute the given target from scratch.
+
+        We throw out everything we learned about the target during its
+        last run, because that information is out of date: we will now
+        relearn what inputs it needs by watching it run all over again.
+
+        """
         self.graph.clear_dependencies_of(target)
         self.target_stack.append(target)
         try:
@@ -35,6 +60,13 @@ class Builder:
         return value
 
     def rebuild(self):
+        """Rebuild everything.
+
+        If nothing has changed recently, the cache's to-do list will be
+        empty, and this call is a no-op.  Otherwise we start running
+        through the to-do list over and over until things stop changing.
+
+        """
         todo = self.cache.todo()
         while todo:
             todo = list(todo)
