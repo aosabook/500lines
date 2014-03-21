b535bb9dca8e221efb2b34c4b4bd3ba0ddac060b
Brandon Rhodes <brandon@rhodesmill.org>
2014-03-21 06:58:21 -0400

    Remove redundant copy of class "Base" from example

--- contingent/example.py
+++ contingent/example.py
@@ -1,40 +1,8 @@
 """Rough experiment from which to derive the design of `contingent`."""
 
+from blog_project import Base, compute
 from builderlib import Builder
-from functools import wraps
-from inspect import ismethod
 from operator import attrgetter
-
-
-class Base:
-    def __getattribute__(self, name):
-        if name.startswith('_'):
-            return object.__getattribute__(self, name)
-        target = (self, name)
-        return self._builder.get(target)
-
-    def __setattr__(self, name, value):
-        if not name.startswith('_'):
-            target = (self, name)
-            self._builder.set(target, value)
-        return object.__setattr__(self, name, value)
-
-
-def compute(target, get):
-    if isinstance(target[1], str):
-        obj, attribute_name = target
-        value = object.__getattribute__(obj, attribute_name)
-        if not ismethod(value):
-            return value
-        method = value
-        @wraps(method)
-        def wrapper(*args):
-            target = (method, args)
-            return get(target)
-        return wrapper
-    else:
-        method, args = target
-        return method(*args)
 
 
 class Blog(Base):
