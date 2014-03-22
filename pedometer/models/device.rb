0f8761ede4b270fa4b28e9a922d46bcc58c2a275
Dessy Daskalov <dessy.daskalov@gmail.com>
2014-03-22 13:38:05 -0400

    Remove data from Device

--- pedometer/models/device.rb
+++ pedometer/models/device.rb
@@ -2,37 +2,16 @@ class Device
 
   # TODO:
   # - Trial should be description? Method isn't great? State, ...
-  attr_reader :data, :rate, :method, :steps, :trial, :format
+  attr_reader :rate, :method, :steps, :trial
 
-  def initialize(data = nil, rate = nil, method = nil, steps = nil, trial = nil)
+  def initialize(rate = nil, method = nil, steps = nil, trial = nil)
     rate_param = rate.to_f.round
     steps_param = steps.to_f.round
 
-    @data   = data
     @rate   = (rate_param > 0) ? rate_param : 100
     @method = method
     @steps  = steps_param if steps_param > 0
     @trial  = trial
-
-    set_format
-  end
-
-private
-
-  # TODO: 
-  # - Does this belong here? 
-  # - Can blow up on parsing, don't need to make sure it's in the right format
-  def set_format
-    any_decimal = '-?\d+(?:\.\d+)?'
-    regexp_accl = Regexp.new('^((' + any_decimal + ',){2}' + 
-                                              any_decimal + ';)+$')
-    regexp_grav = Regexp.new('^(((' + any_decimal + ',){2}(' +
-                                         any_decimal + ')){1}\|(' +
-                                         any_decimal + ',){2}(' +
-                                         any_decimal + ';){1})+$')
-    @format = 'accelerometer' if regexp_accl.match(@data)
-    @format = 'gravity'       if regexp_grav.match(@data)
-    raise "Bad Input. Ensure accelerometer or gravity data is properly formatted." unless @format
   end
 
 end
--- pedometer/test/unit/device_test.rb
+++ pedometer/test/unit/device_test.rb
@@ -4,100 +4,43 @@ require_relative '../../models/device'
 class DeviceTest < Test::Unit::TestCase
 
   def test_create
-    input_data = '0.123,-0.123,5;'
-    device = Device.new(input_data, 5)
+    device = Device.new(5, 'walk', '10', 'test trial 1')
+    assert_equal 5,              device.rate
+    assert_equal 'walk',         device.method
+    assert_equal 10,             device.steps
+    assert_equal 'test trial 1', device.trial
+  end
 
-    assert_equal input_data, device.data
-    assert_equal 5, device.rate
+  def test_create_empty
+    device = Device.new
+    assert_equal 100, device.rate
     assert_nil device.method
     assert_nil device.steps
     assert_nil device.trial
   end
 
-  def test_create_with_method_steps_trial
-    input_data = '0.123,-0.123,5;'
-    
-    device = Device.new(input_data, nil, 'walk', '10', 'test trial 1')
-    assert_equal 'walk', device.method
-    assert_equal 10, device.steps
-    assert_equal 'test trial 1', device.trial
-  end
-
   def test_create_with_rate
-    input = '0.123,-0.123,5;'
-
-    assert_equal 100, Device.new(input).rate
-    assert_equal 100, Device.new(input, nil).rate
-    assert_equal 100, Device.new(input, 'bad rate').rate
-    assert_equal 100, Device.new(input, 0).rate
-    assert_equal 100, Device.new(input, -1).rate
+    assert_equal 100, Device.new().rate
+    assert_equal 100, Device.new(nil).rate
+    assert_equal 100, Device.new('bad rate').rate
+    assert_equal 100, Device.new(0).rate
+    assert_equal 100, Device.new(-1).rate
     
-    assert_equal 2, Device.new(input, '2').rate
-    assert_equal 2, Device.new(input, 2).rate
-    assert_equal 2, Device.new(input, 2.0).rate
-    assert_equal 2, Device.new(input, 1.7).rate
-    assert_equal 1, Device.new(input, 1.2).rate
+    assert_equal 2, Device.new('2').rate
+    assert_equal 2, Device.new(2).rate
+    assert_equal 2, Device.new(2.0).rate
+    assert_equal 2, Device.new(1.7).rate
+    assert_equal 1, Device.new(1.2).rate
   end
 
-  def test_create_accelerometer_data
-    input = '0.123,-0.123,5;0.456,-0.789,0.111;-0.212,0.001,1;'
-    device = Device.new(input)
-    assert_equal 'accelerometer', device.format
-  end
+  def test_create_with_steps
+    assert_nil Device.new(nil, nil, 0).steps
+    assert_nil Device.new(nil, nil, -1).steps
+    assert_nil Device.new(nil, nil, '0').steps
+    assert_nil Device.new(nil, nil, '-1').steps
 
-  def test_create_gravity_data
-    input = '0.028,-0.072,5|0.129,-0.945,-5;0,-0.07,0.06|0.123,-0.947,5;0.2,-1,2|0.1,-0.9,3;'
-    device = Device.new(input)
-    assert_equal 'gravity', device.format
-  end
-
-  # -- Creation Failure Tests -----------------------------------------------
-
-  def test_create_nil_input
-    message = "Bad Input. Ensure accelerometer or gravity data is properly formatted."
-    assert_raise_with_message(RuntimeError, message) do
-      Device.new(nil)
-    end
-  end
-
-  def test_create_empty_input
-    message = "Bad Input. Ensure accelerometer or gravity data is properly formatted."
-    assert_raise_with_message(RuntimeError, message) do
-      Device.new('')
-    end
-  end
-
-  def test_create_bad_input_strings
-    message = "Bad Input. Ensure accelerometer or gravity data is properly formatted."
-    assert_raise_with_message(RuntimeError, message) do
-      Device.new("0.123,-0.123,5;a,b,c;")
-    end
-
-    assert_raise_with_message(RuntimeError, message) do
-      Device.new("0.028,-0.072,a|0.129,-0.945,-5;0,-0.07,0.06|b,-0.947,5;")
-    end
-  end
-
-  def test_create_bad_input_too_many_values
-    message = "Bad Input. Ensure accelerometer or gravity data is properly formatted."
-    assert_raise_with_message(RuntimeError, message) do
-      Device.new("0.123,-0.123,5;0.123,-0.123,5,9;")
-    end
-
-    assert_raise_with_message(RuntimeError, message) do
-      Device.new("0.028,-0.072,5,6|0.129,-0.945,-5;0,-0.07,0.06|0.123,-0.947,5;")
-    end
-  end
-
-  def test_create_bad_input_too_few_values
-    message = "Bad Input. Ensure accelerometer or gravity data is properly formatted."
-    assert_raise_with_message(RuntimeError, message) do
-      Device.new("0.123,-0.123,5;0.123,-0.123;")
-    end
-
-    assert_raise_with_message(RuntimeError, message) do
-      Device.new("0.028,-0.072,5|0.129,-0.945,-5;0,-0.07,0.06|0.123,-0.947;")
-    end
+    assert_equal 10, Device.new(nil, nil, 10).steps
+    assert_equal 1,  Device.new(nil, nil, 0.5).steps
   end
 
 end
