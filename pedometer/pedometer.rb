6e3314ba383840faf9dda0faf247005a4c16d209
Dessy Daskalov <dessy.daskalov@gmail.com>
2014-03-29 13:57:06 -0400

    Fix rescues in controller

--- pedometer/pedometer.rb
+++ pedometer/pedometer.rb
@@ -5,8 +5,6 @@ Dir['./models/*', './helpers/*'].each {|file| require_relative file }
 include FileUtils::Verbose
 
 # TODO: 
-# - Capture exceptions and redirect to /trials
-# - Bad rescues
 # - Make device_params shorter
 post '/create' do
   begin
@@ -20,41 +18,37 @@ post '/create' do
 
     set_match_filtered_data
 
-    erb :detail
+    erb :trial
   rescue Exception => e
-    [400, e.message]
+    redirect '/trials?error=creation'
   end
 end
 
 get '/trials' do
-  begin
-    @data = []
-    files = Dir.glob(File.join('public/uploads', "*"))
-    files.each do |file|
-      user_params, device_params = FileHelper.parse_file_name(file)
-      build_with_params(File.read(file), user_params, device_params)
-
-      @data << {:file => file, :analyzer => @analyzer}
-    end
-
-    erb :data
-  rescue Exception => e
-    [400, e.message]
+  @error = if params[:error]
+    "A #{params[:error]} error has occurred. Please try again."
   end
+
+  @data = []
+  files = Dir.glob(File.join('public/uploads', "*"))
+  files.each do |file|
+    user_params, device_params = FileHelper.parse_file_name(file)
+    build_with_params(File.read(file), user_params, device_params)
+
+    @data << {:file => file, :analyzer => @analyzer}
+  end
+
+  erb :trials
 end
 
 get '/trial/*' do
-  begin
-    @file_name = params[:splat].first
-    user_params, device_params = FileHelper.parse_file_name(@file_name)
-    build_with_params(File.read(@file_name), user_params, device_params)
+  @file_name = params[:splat].first
+  user_params, device_params = FileHelper.parse_file_name(@file_name)
+  build_with_params(File.read(@file_name), user_params, device_params)
 
-    set_match_filtered_data
+  set_match_filtered_data
 
-    erb :detail
-  rescue Exception => e
-    [400, e.message]
-  end
+  erb :trial
 end
 
 def build_with_params(data, user_params, device_params)
