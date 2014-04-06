b4c1e8c91e85ce355a170a3933d43b4823ad35cb
Marina Samuel <msamuel@mozilla.com>
2014-04-05 22:12:20 -0400

    Add js client to accept user input and send it to a python server to learn and make predictions.

---
+++ ocr/ocr.js
@@ -0,0 +1,120 @@
+const BATCH_SIZE = 1
+
+var ctx, canvas;
+var data = [];
+var trainArray = [];
+var trainingRequestCount = 0;
+
+function onLoadFunction() {
+	canvas = document.getElementById('canvas');
+	ctx = canvas.getContext('2d');
+  resetCanvas();
+
+  ctx.fillSquare = fillSquare;
+	canvas.onmousemove = onMouseMove;
+  canvas.onmousedown = onMouseDown;
+  canvas.onmouseup = onMouseUp;
+}
+
+function resetCanvas() {
+  data = [];
+  ctx.fillStyle = '#000000';
+  ctx.fillRect(0, 0, 200, 200);
+  var matrixSize = 400;
+  while (matrixSize--) data.push(0);
+  drawGrid();
+}
+
+
+function drawGrid() {
+  for (var x = 10, y = 10; x < 200; x += 10, y += 10) {
+    ctx.strokeStyle = '#0000ff';
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, 200);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(200, y);
+    ctx.stroke();
+  }
+}
+
+function onMouseMove(e) {
+  if (!canvas.isDrawing) {
+    return;
+  }
+  ctx.fillSquare(e.clientX - this.offsetLeft, e.clientY - this.offsetTop);
+}
+
+function onMouseDown(e) {
+  canvas.isDrawing = true;
+  ctx.fillSquare(e.clientX - this.offsetLeft, e.clientY - this.offsetTop);
+}
+
+function onMouseUp(e) {
+  canvas.isDrawing = false;
+}
+
+function fillSquare(x, y) {
+  var xPixel = Math.floor(x / 10);
+  var yPixel = Math.floor(y / 10);
+  data[((xPixel - 1)  * 20 + yPixel) - 1] = 1;
+
+  this.fillStyle = '#ffffff';
+  this.fillRect(xPixel * 10, yPixel * 10, 10, 10);
+}
+
+function train() {
+  var digitVal = document.getElementById("digit").value;
+  if (!digitVal) {
+    alert("Please type a digit value in order to train the network");
+    return;
+  }
+  trainArray.push({y0: data, label: parseInt(digitVal)});
+  trainingRequestCount++;
+  
+  // Time to send a training batch to the server.
+  if (trainingRequestCount == BATCH_SIZE) {
+    alert("Sending training data to server...");
+    var json = {
+      trainArray: trainArray,
+      train: true
+    };
+
+    sendData(json);
+    trainingRequestCount = 0;
+    trainArray = [];
+  }
+}
+
+function test() {
+  var json = {
+    image: data,
+    train: false
+  };
+  sendData(json);
+}
+
+function receiveResponse() {
+  var responseJSON = JSON.parse(this.responseText);
+  if (this.responseText && responseJSON.type == "test") {
+    alert("The neural network predicts you wrote a \'" + responseJSON.result + '\'');
+  }
+}
+
+function onError(e) {
+  alert("Error occurred while connecting to server: " + e.target.statusText);
+}
+
+function sendData(json) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open('POST', "http://localhost:80", false);
+  xmlHttp.onload = receiveResponse;
+  xmlHttp.onerror = onError;
+  var msg = JSON.stringify(json);
+  xmlHttp.setRequestHeader('Content-length', msg.length);
+  xmlHttp.setRequestHeader("Connection", "close");
+  xmlHttp.send(msg);
+}
--- ocr/ocr.py
+++ ocr/ocr.py
@@ -6,92 +6,125 @@ from numpy import matrix
 from math import pow
 import math
 import random
+import os.path
+import json
 
-# Shuffle the indices of the 5000 samples and use the first 3500 for
-# training and the rest for testing.
-sampleIndices = list(range(5000))
-random.shuffle(sampleIndices)
+class ocrNN:
+	# Shuffle the indices of the 5000 samples and use the first 3500 for
+	# training and the rest for testing.
+	sampleIndices = list(range(5000))
+	random.shuffle(sampleIndices)
 
-learningRate = 0.1
+	LEARNING_RATE = 0.1
+	NN_FILE_PATH = 'nn.json'
 
-# Load data samples and labels into matrix
-dataMatrix = np.loadtxt(open('data.csv', 'rb'), delimiter = ',')
-dataLabels = np.loadtxt(open('dataLabels.csv', 'rb'))
+	# Load data samples and labels into matrix
+	dataMatrix = np.loadtxt(open('data.csv', 'rb'), delimiter = ',')
+	dataLabels = np.loadtxt(open('dataLabels.csv', 'rb'))
 
-# Convert from numpy ndarrays to python lists
-dataMatrix = dataMatrix.tolist()
-dataLabels = dataLabels.tolist()
+	# Convert from numpy ndarrays to python lists
+	dataMatrix = dataMatrix.tolist()
+	dataLabels = dataLabels.tolist()
 
-def randInitializeWeights(sizeIn, sizeOut):
-	return [x * 0.12 - 0.06 for x in np.random.rand(sizeOut, sizeIn)]
+	def randInitializeWeights(self, sizeIn, sizeOut):
+		return [x * 0.12 - 0.06 for x in np.random.rand(sizeOut, sizeIn)]
 
-# The sigmoid activation function
-def sigmoid(z):
-	return 1 / (1 + math.e ** -z)
+	# The sigmoid activation function
+	def sigmoid(self, z):
+		return 1 / (1 + math.e ** -z)
 
-def sigmoidPrime(z):
-	return sigmoid(z) * (1 - sigmoid(z))
+	def sigmoidPrime(self, z):
+		return self.sigmoid(z) * (1 - self.sigmoid(z))
 
-def train(theta1, theta2, b1, b2):
-	for i in sampleIndices[:3500]:
-		y0 = dataMatrix[i]
-		label = int(dataLabels[i])
+	def draw(self, sample):
+		pixelArray = [sample[j:j+20] for j in xrange(0, len(sample), 20)]
+		plt.imshow(zip(*pixelArray), cmap = cm.Greys_r, interpolation="nearest")
+		plt.show()
 
-		# Step 2: Forward propagation
-		y1 = np.dot(np.mat(theta1), np.mat(y0).T)
-		sum1 =  y1 + np.mat(b1) # Add the bias
-		y1 = sigmoid(sum1)
+	def train(self, trainingDataArray):
+		for data in trainingDataArray:
+			# Step 2: Forward propagation
+			y1 = np.dot(np.mat(self.theta1), np.mat(data['y0']).T)
+			sum1 =  y1 + np.mat(self.b1) # Add the bias
+			y1 = self.sigmoid(sum1)
 
-		y2 = np.dot(np.array(theta2), y1)
-		y2 = np.add(y2, b2) # Add the bias
-		y2 = sigmoid(y2)
+			y2 = np.dot(np.array(self.theta2), y1)
+			y2 = np.add(y2, self.b2) # Add the bias
+			y2 = self.sigmoid(y2)
 
-		# Step 3: Back propagation
-		actualVals = [0] * 10
-		actualVals[label] = 1
-		outputErrors = np.mat(actualVals).T - np.mat(y2)
-		hiddenErrors = np.multiply(np.dot(np.mat(theta2).T, outputErrors), sigmoidPrime(sum1))
+			# Step 3: Back propagation
+			actualVals = [0] * 10
+			actualVals[data['label']] = 1
+			outputErrors = np.mat(actualVals).T - np.mat(y2)
+			hiddenErrors = np.multiply(np.dot(np.mat(self.theta2).T, outputErrors), self.sigmoidPrime(sum1))
 
-		# Step 4: Update weights
-		theta1 += learningRate * np.dot(np.mat(hiddenErrors), np.mat(y0))
-		theta2 += learningRate * np.dot(np.mat(outputErrors), np.mat(y1).T)
-		b2 += learningRate * outputErrors
-		b1 += learningRate * hiddenErrors
+			# Step 4: Update weights
+			self.theta1 += ocrNN.LEARNING_RATE * np.dot(np.mat(hiddenErrors), np.mat(data['y0']))
+			self.theta2 += ocrNN.LEARNING_RATE * np.dot(np.mat(outputErrors), np.mat(y1).T)
+			self.b2 += ocrNN.LEARNING_RATE * outputErrors
+			self.b1 += ocrNN.LEARNING_RATE * hiddenErrors
 
-	return (theta1, theta2, b1, b2)
+	def test(self):
+		avgSum = 0
+		for j in range(100):
+			correctGuessCount = 0
+			for i in ocrNN.sampleIndices[3500:]:
+				test = ocrNN.dataMatrix[i]
+				prediction = self.predict(test)
+				if ocrNN.dataLabels[i] == prediction:
+					correctGuessCount += 1
 
-def test(theta1, theta2, b1, b2):
-	avgSum = 0
-	for j in range(100):
-		correctGuessCount = 0
-		for i in sampleIndices[3500:]:
-			test = dataMatrix[i]
-			y1 = np.dot(np.mat(theta1), np.mat(test).T)
-			y1 =  y1 + np.mat(b1) # Add the bias
-			y1 = sigmoid(y1)
+			avgSum += (correctGuessCount / float(1500))
+		print avgSum / float(100)
 
-			y2 = np.dot(np.array(theta2), y1)
-			y2 = np.add(y2, b2) # Add the bias
-			y2 = sigmoid(y2)
+	def predict(self, test):
+		y1 = np.dot(np.mat(self.theta1), np.mat(test).T)
+		y1 =  y1 + np.mat(self.b1) # Add the bias
+		y1 = self.sigmoid(y1)
 
-			results = y2.T.tolist()[0]
-			if dataLabels[i] == results.index(max(results)):
-				correctGuessCount += 1
+		y2 = np.dot(np.array(self.theta2), y1)
+		y2 = np.add(y2, self.b2) # Add the bias
+		y2 = self.sigmoid(y2)
 
-		avgSum += (correctGuessCount / float(1500))
-	print avgSum / float(100)
+		results = y2.T.tolist()[0]
+		return results.index(max(results))
 
+	def normalize(self, intensity, newMax, newMin):
+		return intensity * (float(newMax - newMin) / float(255)) + newMin
 
-if __name__ == "__main__":
-	sigmoid = np.vectorize(sigmoid)
-	sigmoidPrime = np.vectorize(sigmoidPrime)
+	def save(self):
+		nnFile = open(ocrNN.NN_FILE_PATH,'w');
+		json.dump({
+			"theta1":[npArr.tolist() for npArr in self.theta1],
+			"theta2":[npArr.tolist() for npArr in self.theta2],
+			"b1":self.b1[0].tolist(),
+			"b2":self.b2[0].tolist()
+		}, nnFile)
+		nnFile.close()
 
-	for j in range(5, 100, 5):		
-		# Step 1: Initialize weights to small numbers
-		theta1 = randInitializeWeights(400, j)
-		theta2 = randInitializeWeights(j, 10)
-		b1 = randInitializeWeights(1, j)
-		b2 = randInitializeWeights(1, 10)
+	def load(self):
+		nnFile = open(ocrNN.NN_FILE_PATH);
+		nn = json.load(nnFile)
+		self.theta1 = [np.array(li) for li in nn['theta1']]
+		self.theta2 = [np.array(li) for li in nn['theta2']]
+		self.b1 = [np.array(nn['b1'][0])]
+		self.b2 = [np.array(nn['b2'][0])]
+		nnFile.close()
 
-		theta1, theta2, b1, b2 = train(theta1, theta2, b1, b2)
-		test(theta1, theta2, b1, b2)
+	def __init__(self, numHiddenNodes):
+		self.sigmoid = np.vectorize(self.sigmoid)
+		self.sigmoidPrime = np.vectorize(self.sigmoidPrime)
+
+		if (not os.path.isfile('nn.json')):
+			# Step 1: Initialize weights to small numbers
+			self.theta1 = self.randInitializeWeights(400, numHiddenNodes)
+			self.theta2 = self.randInitializeWeights(numHiddenNodes, 10)
+			self.b1 = self.randInitializeWeights(1, numHiddenNodes)
+			self.b2 = self.randInitializeWeights(1, 10)
+
+			# Train using sample data
+			self.train([{"y0":self.dataMatrix[i], "label":int(ocrNN.dataLabels[i])} for i in ocrNN.sampleIndices[:3500]])
+			self.test()
+			self.save()
+		else:
+			self.load()
