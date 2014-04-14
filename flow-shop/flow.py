bba9e303724528348191104a1b90d61091d6215f
Christian Muise <christian.muise@gmail.com>
2014-04-14 15:02:36 +1000

    Put in a number of changes suggested in the code review. Includes a restructuring of the code, better error handling, detailed explanations, etc.

--- flow-shop/flow.py
+++ flow-shop/flow.py
@@ -1,7 +1,11 @@
 import sys, os, time, random
 
 from functools import partial
-from itertools import combinations, permutations, product
+from collections import namedtuple
+from itertools import product
+
+import neighbourhood as neigh
+import heuristics as heur
 
 ##############
 ## Settings ##
@@ -12,169 +16,189 @@ DEBUG_SWITCH = False # Displays intermediate heuristic info when True
 MAX_LNS_NEIGHBOURHOODS = 1000 # Maximum number of neighbours to explore in LNS
 
 
-##############################
-## Neighbourhood Generators ##
-##############################
-
-def _neighbours_random(data, perm, num = 1):
-    # Returns <num> random job permutations, including the current one
-    candidates = [perm]
-    for i in range(num):
-        candidate = perm[:]
-        random.shuffle(candidate)
-        candidates.append(candidate)
-    return candidates
-
-def _neighbours_swap(data, perm):
-    # Returns the permutations corresponding to swapping every pair of jobs
-    candidates = [perm]
-    for (i,j) in combinations(range(len(perm)), 2):
-        candidate = perm[:]
-        candidate[i], candidate[j] = candidate[j], candidate[i]
-        candidates.append(candidate)
-    return candidates
-
-def _neighbours_LNS(data, perm, size = 2):
-    # Returns the Large Neighbourhood Search neighbours
-    candidates = [perm]
-
-    # Bound the number of neighbourhoods in case there are too many jobs
-    neighbourhoods = list(combinations(range(len(perm)), size))
-    random.shuffle(neighbourhoods)
-
-    for subset in neighbourhoods[:MAX_LNS_NEIGHBOURHOODS]:
-
-        # Keep track of the best candidate for each neighbourhood
-        best_make = makespan(data, perm)
-        best_perm = perm
-
-        # Enumerate every permutation of the selected neighbourhood
-        for ordering in permutations(subset):
-            candidate = perm[:]
-            for i in range(len(ordering)):
-                candidate[subset[i]] = perm[ordering[i]]
-            res = makespan(data, candidate)
-            if res < best_make:
-                best_make = res
-                best_perm = candidate
-
-        # Record the best candidate as part of the larger neighbourhood
-        candidates.append(best_perm)
-
-    return candidates
-
-def _neighbours_idle(data, perm, size=4):
-    # Returns the permutations of the most <size> idle jobs
-    candidates = [perm]
-
-    # Compute the idle time for each job
-    sol = compile_solution(data, perm)
-    results = []
-
-    for i in range(len(data)):
-        finish_time = sol[-1][i] + data[perm[i]][-1]
-        idle_time = (finish_time - sol[0][i]) - sum([time for time in data[perm[i]]])
-        results.append((idle_time, perm[i]))
-
-    # Take the <size> most idle jobs
-    subset = [job for (idle, job) in list(reversed(results))[:size]]
-
-    # Enumerate the permutations of the idle jobs
-    for ordering in permutations(subset):
-        candidate = perm[:]
-        for i in range(len(ordering)):
-            candidate[subset[i]] = perm[ordering[i]]
-        candidates.append(candidate)
-
-    return candidates
-
-
 ################
-## Heuristics ##
-################
+## Strategies ##
+#################################################
+## A strategy is a particular configuration
+##  of neighbourhood generator (to compute
+##  the next set of candidates) and heuristic
+##  computation (to select the best candidate).
+##
 
-################################################################
-## A heuristic returns a single candidate permutation from
-##  a set of candidates that is given. The heuristic is also
-##  given access to the problem data in order to evaluate
-##  which candidate might be preferred.
-
-def _heur_hillclimbing(data, candidates):
-    # Returns the best candidate in the list
-    scores = [(makespan(data, perm), perm) for perm in candidates]
-    return sorted(scores)[0][1]
-
-def _heur_random(data, candidates):
-    # Returns a random candidate choice
-    return random.choice(candidates)
-
-def _heur_random_hillclimbing(data, candidates):
-    # Returns a candidate with probability proportional to its rank in sorted quality
-    scores = [(makespan(data, perm), perm) for perm in candidates]
-    i = 0
-    while (random.random() < 0.5) and (i < len(scores) - 1):
-        i += 1
-    return sorted(scores)[i][1]
-
-
-################################
-
-# Define the neighbourhoods (and parameters) we would like to investigate
-NEIGHBOURHOODS = [
-    ('Random Permutation', partial(_neighbours_random, num=100)),
-    ('Swapped Pairs', _neighbours_swap),
-    ('Large Neighbourhood Search (2)', partial(_neighbours_LNS, size=2)),
-    ('Large Neighbourhood Search (3)', partial(_neighbours_LNS, size=3)),
-    ('Idle Neighbourhood (3)', partial(_neighbours_idle, size=3)),
-    ('Idle Neighbourhood (4)', partial(_neighbours_idle, size=4)),
-    ('Idle Neighbourhood (5)', partial(_neighbours_idle, size=5))
-]
-
-# Define the heuristics we would like to investigate
-HEURISTICS = [
-    ('Hill Climbing', _heur_hillclimbing),
-    ('Random Selection', _heur_random),
-    ('Biased Random Selection', _heur_random_hillclimbing)
-]
-
-# Combine every neighbourhood and heuristic strategy
 STRATEGIES = []
-for (n, h) in product(NEIGHBOURHOODS, HEURISTICS):
-    STRATEGIES.append({'name': "%s / %s" % (n[0], h[0]), # Unique name
-                       'neigh': n[1], # Neighbourhood function
-                       'heur': h[1], # Heuristic function
-                       'weight': 1, # Weight to determine chance of usage
-                       'usage': 0}) # Number of times the strategy is used
+
+# Using a namedtuple is a little cleaner than dictionaries.
+#  E.g., strategy['name'] versus strategy.name
+Strategy = namedtuple('Strategy', ['name', 'neighbourhood', 'heuristic'])
+
+def initialize_strategies():
+
+    global STRATEGIES
+
+    # Define the neighbourhoods (and parameters) we would like to use
+    NEIGHBOURHOODS = [
+        ('Random Permutation', partial(neigh.neighbours_random, num=100)),
+        ('Swapped Pairs', neigh.neighbours_swap),
+        ('Large Neighbourhood Search (2)', partial(neigh.neighbours_LNS, size=2)),
+        ('Large Neighbourhood Search (3)', partial(neigh.neighbours_LNS, size=3)),
+        ('Idle Neighbourhood (3)', partial(neigh.neighbours_idle, size=3)),
+        ('Idle Neighbourhood (4)', partial(neigh.neighbours_idle, size=4)),
+        ('Idle Neighbourhood (5)', partial(neigh.neighbours_idle, size=5))
+    ]
+
+    # Define the heuristics we would like to use
+    HEURISTICS = [
+        ('Hill Climbing', heur.heur_hillclimbing),
+        ('Random Selection', heur.heur_random),
+        ('Biased Random Selection', heur.heur_random_hillclimbing)
+    ]
+
+    # Combine every neighbourhood and heuristic strategy
+    for (n, h) in product(NEIGHBOURHOODS, HEURISTICS):
+        STRATEGIES.append(Strategy("%s / %s" % (n[0], h[0]), n[1], h[1]))
 
 
-def _pick_strategy(strategies):
-    # Picks a random strategy based on its weight: roulette wheel selection
-    total = sum([strat['weight'] for strat in strategies])
-    pick = random.uniform(0, total)
-    count = strategies[0]['weight']
 
-    i = 0
-    while pick > count:
-        count += strategies[i+1]['weight']
-        i += 1
+def solve(data):
+    """Solves an instance of the flow shop scheduling problem"""
 
-    return (strategies[i],i)
+    # We initialize the strategies here to avoid cyclic import issues
+    initialize_strategies()
+    global STRATEGIES
+
+    # Record the following for each strategy:
+    #  improvements: The amount a solution was improved by this strategy
+    #  time_spent: The amount of time spent on the strategy
+    #  weights: The weights that correspond to how good a strategy is
+    #  usage: The number of times we use a strategy
+    strat_improvements = {strategy: 0 for strategy in STRATEGIES}
+    strat_time_spent = {strategy: 0 for strategy in STRATEGIES}
+    strat_weights = {strategy: 1 for strategy in STRATEGIES}
+    strat_usage = {strategy: 0 for strategy in STRATEGIES}
+
+    # Start with a random permutation of the jobs
+    perm = range(len(data))
+    random.shuffle(perm)
+
+    # Keep track of the best solution
+    best_make = makespan(data, perm)
+    best_perm = perm
+    res = best_make
+
+    # Maintain statistics and timing for the iterations
+    iteration = 0
+    time_limit = time.time() + TIME_LIMIT
+    time_last_switch = time.time()
+
+    time_delta = TIME_LIMIT / 10
+    checkpoint = time.time() + time_delta
+    percent_complete = 10
+
+    print "\nSolving..."
+
+    while time.time() < time_limit:
+
+        if time.time() > checkpoint:
+            print " %d %%" % percent_complete
+            percent_complete += 10
+            checkpoint += time_delta
+
+        iteration += 1
+
+        # Heuristically choose the best strategy
+        strategy = pick_strategy(STRATEGIES, strat_weights)
+
+        old_val = res
+        old_time = time.time()
+
+        # Use the current strategy's heuristic to pick the next permutation from
+        #  the set of candidates generated by the strategy's neighbourhood
+        candidates = strategy.neighbourhood(data, perm)
+        perm = strategy.heuristic(data, candidates)
+        res = makespan(data, perm)
+
+        # Record the statistics on how the strategy did
+        strat_improvements[strategy] += res - old_val
+        strat_time_spent[strategy] += time.time() - old_time
+        strat_usage[strategy] += 1
+
+        if res < best_make:
+            print "New best (%d)" % res
+            best_make = res
+            best_perm = perm[:]
+
+        # At regular intervals, switch the weighting on the strategies available.
+        #  This way, the search can dynamically shift towards strategies that have
+        #  proven more effective recently.
+        if time.time() > time_last_switch + TIME_INCREMENT:
+
+            # Normalize the improvements made by the time it takes to make them
+            results = sorted([(float(strat_improvements[s]) / max(0.001, strat_time_spent[s]), s)
+                              for s in STRATEGIES])
+
+            if DEBUG_SWITCH:
+                print "\nComputing another switch..."
+                print "Best performer: %s (%d)" % (results[0][1].name, results[0][0])
+                print "Worst performer: %s (%d)" % (results[-1][1].name, results[-1][0])
+
+            # Boost the weight for the successful strategies
+            for i in range(len(STRATEGIES)):
+                strat_weights[results[i][1]] += len(STRATEGIES) - i
+
+                # Additionally boost the unused strategies to avoid starvation
+                if 0 == results[i][0]:
+                    strat_weights[results[i][1]] += len(STRATEGIES)
+
+            time_last_switch = time.time()
+
+            if DEBUG_SWITCH:
+                print results
+                print sorted([strat_weights[STRATEGIES[i]] for i in range(len(STRATEGIES))])
+
+            strat_improvements = {strategy: 0 for strategy in STRATEGIES}
+            strat_time_spent = {strategy: 0 for strategy in STRATEGIES}
 
 
-def parse_problem(filename):
-    """Parse the first instance of a Taillard problem file"""
+    print " %d %%\n" % percent_complete
+    print "\nWent through %d iterations." % iteration
+
+    print "\n(usage) Strategy:"
+    results = sorted([(strat_weights[STRATEGIES[i]], i)
+                      for i in range(len(STRATEGIES))], reverse=True)
+    for (w, i) in results:
+        print "(%d) \t%s" % (strat_usage[STRATEGIES[i]], STRATEGIES[i].name)
+
+    return (best_perm, best_make)
+
+
+def parse_problem(filename, k=1):
+    """Parse the kth instance of a Taillard problem file
+
+    The Taillard problem files are a standard benchmark set for the problem
+    of flow shop scheduling. They can be found online at the following address:
+    - http://mistic.heig-vd.ch/taillard/problemes.dir/ordonnancement.dir/ordonnancement.html"""
 
     print "\nParsing..."
 
     with open(filename, 'r') as f:
         # Identify the string that separates instances
-        problem_line = 'number of jobs, number of machines, initial seed, upper bound and lower bound :'
+        problem_line = '/number of jobs, number of machines, initial seed, upper bound and lower bound :/'
 
         # Strip spaces and newline characters from every line
         lines = map(str.strip, f.readlines())
 
-        # We know the first instance is from line 3 to the start of instance 2
-        lines = lines[3:lines.index(problem_line, 1)]
+        # We know the last line is blank, and we prep the first line for later
+        lines = lines[:-1]
+        lines[0] = '/' + lines[0]
+
+        # We also know '/' does not appear in the files, so we can use it as
+        #  a separator to find the right lines for the kth problem instance
+        try:
+            lines = '/'.join(lines).split(problem_line)[k].split('/')[2:]
+        except IndexError:
+            max_instances = len('/'.join(lines).split(problem_line)) - 1
+            print "\nError: Instance must be within 1 and %d\n" % max_instances
+            sys.exit(0)
 
         # Split every line based on spaces and convert each item to an int
         data = [map(int, line.split()) for line in lines]
@@ -182,6 +206,23 @@ def parse_problem(filename):
     # We return the zipped data to rotate the rows and columns, making each
     #  item in data the durations of tasks for a particular job
     return zip(*data)
+
+
+def pick_strategy(strategies, weights):
+    # Picks a random strategy based on its weight: roulette wheel selection
+    #  Rather than selecting a strategy entirely at random, we bias the
+    #  random selection towards strategies that have worked well in the
+    #  past (according to the weight value).
+    total = sum([weights[strategy] for strategy in strategies])
+    pick = random.uniform(0, total)
+    count = weights[strategies[0]]
+
+    i = 0
+    while pick > count:
+        count += weights[strategies[i+1]]
+        i += 1
+
+    return strategies[i]
 
 
 def makespan(data, perm):
@@ -227,108 +268,6 @@ def compile_solution(data, perm):
     return machine_times
 
 
-def solve(data):
-    """Solves an instance of the flow shop scheduling problem"""
-    global STRATEGIES
-
-    # Record the improvements made by each heuristic and the time they are used
-    improvements = [0] * len(STRATEGIES)
-    time_spent = [0] * len(STRATEGIES)
-
-    # Start with a random permutation of the jobs
-    perm = range(len(data))
-    random.shuffle(perm)
-
-    # Keep track of the best solution
-    best_make = makespan(data, perm)
-    best_perm = perm
-    res = best_make
-
-    # Maintain statistics and timing for the iterations
-    iteration = 0
-    time_limit = time.time() + TIME_LIMIT
-    time_last_switch = time.time()
-
-    time_delta = TIME_LIMIT / 10
-    checkpoint = time.time() + time_delta
-    percent_complete = 10
-
-    print "\nSolving..."
-
-    while time.time() < time_limit:
-
-        if time.time() > checkpoint:
-            print " %d %%" % percent_complete
-            percent_complete += 10
-            checkpoint += time_delta
-
-        iteration += 1
-
-        # Heuristically choose the best strategy
-        (strategy, strategy_index) = _pick_strategy(STRATEGIES)
-
-        old_val = res
-        old_time = time.time()
-
-        # Use the current strategy's heuristic to pick the next permutation from
-        #  the set of candidates generated by the strategy's neighbourhood
-        heuristic = strategy['heur']
-        neighbourhood_generator = strategy['neigh']
-        candidates = neighbourhood_generator(data, perm)
-
-        perm = heuristic(data, candidates)
-        res = makespan(data, perm)
-
-        # Record the statistics on how the strategy did
-        improvements[strategy_index] += res - old_val
-        time_spent[strategy_index] += time.time() - old_time
-        STRATEGIES[strategy_index]['usage'] += 1
-
-        if res < best_make:
-            best_make = res
-            best_perm = perm[:]
-
-        # At regular intervals, switch the weighting on the strategies available.
-        #  This way, the search can dynamically shift towards strategies that have
-        #  proven more effective recently.
-        if time.time() > time_last_switch + TIME_INCREMENT:
-
-            # Normalize the improvements made by the time it takes to make them
-            results = sorted([(float(improvements[i]) / max(0.001, time_spent[i]), i) for i in range(len(STRATEGIES))])
-
-            if DEBUG_SWITCH:
-                print "\nComputing another switch..."
-                print "Best performer: %s (%d)" % (STRATEGIES[results[0][1]]['name'], results[0][0])
-                print "Worst performer: %s (%d)" % (STRATEGIES[results[-1][1]]['name'], results[-1][0])
-
-            # Boost the weight for the successful strategies
-            for i in range(len(STRATEGIES)):
-                STRATEGIES[results[i][1]]['weight'] += len(STRATEGIES) - i
-
-                # Additionally boost the unused strategies to avoid starvation
-                if 0 == results[i][0]:
-                    STRATEGIES[results[i][1]]['weight'] += len(STRATEGIES)
-
-            time_last_switch = time.time()
-
-            if DEBUG_SWITCH:
-                print results
-                print sorted([STRATEGIES[i]['weight'] for i in range(len(STRATEGIES))])
-
-            improvements = [0] * len(STRATEGIES)
-            time_spent = [0] * len(STRATEGIES)
-
-
-    print " %d %%\n" % percent_complete
-    print "\nWent through %d iterations." % iteration
-
-    print "\n(usage) Strategy:"
-    results = sorted([(STRATEGIES[i]['weight'], i) for i in range(len(STRATEGIES))], reverse=True)
-    for (w, i) in results:
-        print "(%d) \t%s" % (STRATEGIES[i]['usage'], STRATEGIES[i]['name'])
-
-    return (best_perm, best_make)
-
 
 def print_solution(data, perm):
     """Prints statistics on the computed solution"""
@@ -361,6 +300,14 @@ def print_solution(data, perm):
 
 
 if __name__ == '__main__':
-    data = parse_problem(sys.argv[1])
+
+    if 2 == len(sys.argv):
+        data = parse_problem(sys.argv[1])
+    elif 3 == len(sys.argv):
+        data = parse_problem(sys.argv[1], int(sys.argv[2]))
+    else:
+        print "\nUsage: python flow.py <Taillard problem file> [<instance number>]\n"
+        sys.exit(0)
+
     (perm, ms) = solve(data)
     print_solution(data, perm)
