8033e7ef864d4a154c2f28dbb37adbb23e0f2b6e
yrubin <yrubin@yrubin-dev.middleeast.corp.microsoft.com>
2014-05-05 10:10:21 +0300

    following @beloglazov comments - better and idiomatic usage of defn- with better naming, thus getting a clearer cut between the public and the private APIs of this namespace.

--- functionalDB/src/core/manage.clj
+++ functionalDB/src/core/manage.clj
@@ -1,21 +1,21 @@
 (ns core.manage
-  "Management of db creation and db connections. This module allows acquire new db or reset an existing one, as well as get the db value from a connection"
+  "Management of db connections (from the user's perspective), internally creates/ / drops dbs."
   (:use core.constructs))
 
-(def __ALL-DBS__ (atom {}))
+(def ^:private __ALL-DBS__ (atom {}))
 
-(defn _get-db  [dbs db-name] (if (db-name dbs)  dbs  (assoc dbs db-name (make-db))))
+(defn- force-get-db  [dbs db-name] (if (db-name dbs)  dbs  (assoc dbs db-name (make-db))))
 
-(defn _reset-db [dbs db-name] (dissoc dbs db-name))
+(defn- drop-db [dbs db-name] (dissoc dbs db-name))
 
-(defn _as-db-name [db-name] (keyword db-name))
+(defn- as-db-name [db-name] (keyword db-name))
 
 (defn get-db-conn [db-name]
-  (let [stored-db-name (_as-db-name db-name)]
-  (stored-db-name (swap! __ALL-DBS__ _get-db stored-db-name))))
+  (let [stored-db-name (as-db-name db-name)]
+  (stored-db-name (swap! __ALL-DBS__ force-get-db stored-db-name))))
 
-(defn reset-db-conn [db-name]
-  (let [stored-db-name (_as-db-name db-name)]
-  (swap! __ALL-DBS__ _reset-db stored-db-name)) nil)
+(defn drop-db-conn [db-name]
+  (let [stored-db-name (as-db-name db-name)]
+  (swap! __ALL-DBS__ drop-db stored-db-name)) nil)
 
 (defn db-from-conn [conn] @conn)
