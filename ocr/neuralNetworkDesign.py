3fe917d5fcc6c9afafcfc4ced6084d70116e9d38
Marina Samuel <msamuel@mozilla.com>
2014-05-18 21:41:36 -0400

    xrange instead of range and remove unecessary float() cast.

--- ocr/neuralNetworkDesign.py
+++ ocr/neuralNetworkDesign.py
@@ -14,7 +14,7 @@ from sklearn.cross_validation import train_test_split
 
 def test(data_matrix, data_labels, test_indices, nn):
     avg_sum = 0
-    for j in range(100):
+    for j in xrange(100):
         correct_guess_count = 0
         for i in test_indices:
             test = data_matrix[i]
@@ -23,7 +23,7 @@ def test(data_matrix, data_labels, test_indices, nn):
                 correct_guess_count += 1
 
         avg_sum += (correct_guess_count / float(len(test_indices)))
-    return avg_sum / float(100)
+    return avg_sum / 100
 
 
 # Load data samples and labels into matrix
