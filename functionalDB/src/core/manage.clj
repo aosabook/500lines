47660ce3622003478985b818bedf96a88d88eff5
Yoav Rubin <yoavrubin@hotmail.com>
2014-05-24 12:11:42 +0300

    renaming force-get to put
    achieving naming that follow the usual idempotence semantics of PUT in
    HTTP calls

--- functionalDB/src/core/manage.clj
+++ functionalDB/src/core/manage.clj
@@ -4,7 +4,7 @@
 
 (def ^:private __ALL-DBS__ (atom {}))
 
-(defn- force-get-db  [dbs db-name] (if (db-name dbs)  dbs  (assoc dbs db-name (make-db))))
+(defn- put-db  [dbs db-name] (if (db-name dbs)  dbs  (assoc dbs db-name (make-db))))
 
 (defn- drop-db [dbs db-name] (dissoc dbs db-name))
 
@@ -12,7 +12,7 @@
 
 (defn get-db-conn [db-name]
   (let [stored-db-name (as-db-name db-name)]
-  (stored-db-name (swap! __ALL-DBS__ force-get-db stored-db-name))))
+  (stored-db-name (swap! __ALL-DBS__ put-db stored-db-name))))
 
 (defn drop-db-conn [db-name]
   (let [stored-db-name (as-db-name db-name)]
