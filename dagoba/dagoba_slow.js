b663c9bbc7387262035e0170e887f016a7dcbe3a
dann toliver <dann@bentobox.net>
2014-06-08 01:37:52 -0400

    perf tests; optimizations (including half of pointerization); etc

--- dagoba/dagoba_slow.js
+++ dagoba/dagoba_slow.js
@@ -1,17 +1,20 @@
-Dagoba = {}
+/// THIS IS THE OLD SLOW STYLE OF DAGOBA DON'T USE IT IT'S FOR DEMONSTRATION ONLY!
 
-Dagoba.Query = {}
 
-Dagoba.Query.add = function(list) {
+DagobaSlow = {}
+
+DagobaSlow.Query = {}
+
+DagobaSlow.Query.add = function(list) {
   this.queue.push(list)
   return this
 }
 
-Dagoba.make_fun = function(name) {
+DagobaSlow.make_fun = function(name) {
   return function() { return this.add([name].concat(Array.prototype.slice.apply(arguments))) } }
 
-Dagoba.query = function(graph) { 
-  var query = Object.create(Dagoba.Query) 
+DagobaSlow.query = function(graph) { 
+  var query = Object.create(DagobaSlow.Query) 
   query.queue = []                  // an array of 
   query.graph = graph
   query.result = null
@@ -22,7 +25,7 @@ Dagoba.query = function(graph) {
   return query
 }
 
-Dagoba.Query.run = function() {
+DagobaSlow.Query.run = function() {
   // for each queue element: invoke the attached function once, pass input to next function, until end
   // after end, back up to each function until you find one that still gives results, then move forward
   // if you reach the beginning and have no results, stop.
@@ -37,7 +40,7 @@ Dagoba.Query.run = function() {
   
   //  var item = this.queue[this.pointer]
   //  var history = this.history[this.pointer] || []
-  // var foo = Dagoba.Funs[item[0]](this.graph, this.gremlins, history, item.slice(1))
+  // var foo = DagobaSlow.Funs[item[0]](this.graph, this.gremlins, history, item.slice(1))
   // this.gremlins = foo.gremlins // ugh this is unnecessary 
   // this.history[this.pointer] = foo.history[this.pointer] // kinda this too (but kinda not)
   
@@ -54,8 +57,8 @@ Dagoba.Query.run = function() {
   
   function stepper(step_index, gremlin) {
     var step = queue[step_index]
-    if(!Dagoba.Funs[step[0]]) return Dagoba.onError('Unrecognized function call: ' + step[0]) || {}
-    return Dagoba.Funs[step[0]](graph, step.slice(1) || {}, gremlin || {}, state[step_index] || {})
+    if(!DagobaSlow.Funs[step[0]]) return DagobaSlow.onError('Unrecognized function call: ' + step[0]) || {}
+    return DagobaSlow.Funs[step[0]](graph, step.slice(1) || {}, gremlin || {}, state[step_index] || {})
   }
   
   function eat_result(step_index, result) {
@@ -77,7 +80,7 @@ Dagoba.Query.run = function() {
   // var args  = step.slice(1)
   // var state = this.state[0]
   // var gremlin = {}
-  // var first = Dagoba.Funs[step[0]](graph, args, gremlin, state)
+  // var first = DagobaSlow.Funs[step[0]](graph, args, gremlin, state)
   // eat_gremlins(0, first)
   
   eat_gremlins(0, stepper(0))
@@ -95,12 +98,12 @@ Dagoba.Query.run = function() {
   var collection = this.state[this.state.length - 1] || []
   this.result = collection.map(function(gremlin) {return gremlin.vertex})
 
-  this.result = Dagoba.firehooks('postquery', this, this.result)[0]
+  this.result = DagobaSlow.firehooks('postquery', this, this.result)[0]
 
   return this.result
 }
 
-Dagoba.Query.name = function() {
+DagobaSlow.Query.name = function() {
   this.add(['name'])
   this.run()
   return this.result
@@ -108,11 +111,11 @@ Dagoba.Query.name = function() {
 }
 
 var methods = ['out', 'in', 'attr', 'outV', 'outE', 'inV', 'inE', 'both', 'bothV', 'bothE', 'filter']
-methods.forEach(function(name) {Dagoba.Query[name] = Dagoba.make_fun(name)})
+methods.forEach(function(name) {DagobaSlow.Query[name] = DagobaSlow.make_fun(name)})
 
-Dagoba.Funs = {
+DagobaSlow.Funs = {
   vertex: function(graph, args, gremlin, state) {
-    // if(!gremlin) gremlin = Dagoba.make_gremlin()
+    // if(!gremlin) gremlin = DagobaSlow.make_gremlin()
     if(!gremlin.state)
       gremlin.state = graph.findVertices(args)
     if(gremlin.state.length == 0)
@@ -120,36 +123,36 @@ Dagoba.Funs = {
     
     var vert = gremlin.state.pop()
     var vertex = graph.findVertexById(vert._id)
-    var clone = Dagoba.make_gremlin(vertex)
+    var clone = DagobaSlow.make_gremlin(vertex)
     return {stay: [gremlin], go: [clone]}
   
     // if(state.status == 'done') return false    
     // var vertices = args[0] ? graph.findVertexById(args[0]) : graph.vertices
-    // var gremlin = Dagoba.make_gremlin(vertex)
+    // var gremlin = DagobaSlow.make_gremlin(vertex)
     // return {stay: [], go: [gremlin], state: {status: 'done'}}
-    // return thread(args[0], graph.findVertexById, Dagoba.make_gremlin, ...)
+    // return thread(args[0], graph.findVertexById, DagobaSlow.make_gremlin, ...)
     // return { gremlins: [{ state: 'alive', path: [graph.findVertexById(args[0])] }], history: ['done'] } 
   },
   
   out: function(graph, args, gremlin, state) {
     if(!gremlin.state)
-      gremlin.state = graph.findOutEdges(gremlin.vertex).filter(Dagoba.filterThings(args[0]))
+      gremlin.state = graph.findOutEdges(gremlin.vertex).filter(DagobaSlow.filterThings(args[0]))
         //function(edge) {return args[0] ? edge._label == args[0] : true})
     if(gremlin.state.length == 0)
       return {} // original gremlin dies here...
     var vertex = graph.findVertexById(gremlin.state.pop()._in)
-    var clone = Dagoba.make_gremlin(vertex)
+    var clone = DagobaSlow.make_gremlin(vertex)
     return {stay: [gremlin], go: [clone]}
   },
   
   'in': function(graph, args, gremlin, state) {
     if(!gremlin.state)
-      gremlin.state = graph.findInEdges(gremlin.vertex).filter(Dagoba.filterThings(args[0]))
+      gremlin.state = graph.findInEdges(gremlin.vertex).filter(DagobaSlow.filterThings(args[0]))
         //function(edge) {return args[0] ? edge._label == args[0] : true})
     if(gremlin.state.length == 0)
       return {} // original gremlin dies here...
     var vertex = graph.findVertexById(gremlin.state.pop()._out)
-    var clone = Dagoba.make_gremlin(vertex)
+    var clone = DagobaSlow.make_gremlin(vertex)
     return {stay: [gremlin], go: [clone]}
   },
   
@@ -164,7 +167,7 @@ Dagoba.Funs = {
 
 
 
-Dagoba.make_gremlin = function(vertex, state) {
+DagobaSlow.make_gremlin = function(vertex, state) {
   return {vertex: vertex, state: state}
 }
 
@@ -199,103 +202,96 @@ ok. realistically, we can
 
 
 
-Dagoba.hooks = {}
-Dagoba.addhook = function(type, callback) {
-  if(!Dagoba.hooks[type]) Dagoba.hooks[type] = []
-  Dagoba.hooks[type].push(callback)
+DagobaSlow.hooks = {}
+DagobaSlow.addhook = function(type, callback) {
+  if(!DagobaSlow.hooks[type]) DagobaSlow.hooks[type] = []
+  DagobaSlow.hooks[type].push(callback)
 }
 
-Dagoba.firehooks = function(type, query) {
+DagobaSlow.firehooks = function(type, query) {
   var args = [].slice.call(arguments, 2)
-  return ((Dagoba.hooks || {})[type] || []).reduce(function(acc, callback) {return callback.apply(query, acc)}, args)
+  return ((DagobaSlow.hooks || {})[type] || []).reduce(function(acc, callback) {return callback.apply(query, acc)}, args)
 }
 
-Dagoba.addhook('postquery', Dagoba.uniqueify = function (results) { // THINK: should we inline this and merge&count in the gremlins?
+DagobaSlow.addhook('postquery', DagobaSlow.uniqueify = function (results) { // THINK: should we inline this and merge&count in the gremlins?
   return [results.filter(function(item, index, array) {return array.indexOf(item) == index})]
 })
 
-Dagoba.addhook('postquery', Dagoba.cleanclone = function (results) { // THINK: do we always want this?
+DagobaSlow.addhook('postquery', DagobaSlow.cleanclone = function (results) { // THINK: do we always want this?
   return [results.map(function(item) {return JSON.parse(JSON.stringify(item, function(key, value) {return key[0]=='_' ? undefined : value}))})]
 })
 
 
 
-Dagoba.Graph = {}
-Dagoba.Graph.v = function() {
-  var query = Dagoba.query(this)
+DagobaSlow.Graph = {}
+DagobaSlow.Graph.v = function() {
+  var query = DagobaSlow.query(this)
   query.add(['vertex'].concat( [].slice.call(arguments) ))
   return query
 }
 
-Dagoba.graph = function() { 
-  var graph = Object.create( Dagoba.Graph ) 
+DagobaSlow.graph = function() { 
+  var graph = Object.create( DagobaSlow.Graph ) 
   graph.vertices = [] // can't stick these on the prototype or they'll be shared
   graph.edges = []
-  graph.vertexIndex = {}
   return graph
 }
 
-Dagoba.Graph.addVertex = function(vertex) {
+DagobaSlow.Graph.addVertex = function(vertex) {
   if(!vertex._id) 
     vertex._id = this.vertices.length+1
   this.vertices.push(vertex)
-  this.vertexIndex[vertex._id] = vertex
-  vertex._out = []; vertex._in = []
 }
 
-Dagoba.Graph.addVertices = function(vertices) {
+DagobaSlow.Graph.addVertices = function(vertices) {
   vertices.forEach(this.addVertex.bind(this))
 }
 
-Dagoba.Graph.addEdge = function(edge) {
+DagobaSlow.Graph.addEdge = function(edge) {
   if(!edge._label) return false
-  edge._in  = this.findVertexById(edge._in)
-  edge._out = this.findVertexById(edge._out)
-  if(!(edge._in && edge._out)) return false
-  edge._in._out.push(edge)
-  edge._in._in.push(edge)
+  if(!this.findVertexById(edge._in)) return false
+  if(!this.findVertexById(edge._out)) return false
   this.edges.push(edge)
 }
 
-Dagoba.Graph.addEdges = function(edges) {
+DagobaSlow.Graph.addEdges = function(edges) {
   edges.forEach(this.addEdge.bind(this))
 }
 
-Dagoba.Graph.findVertexById = function(vertex_id) {
-  return this.vertexIndex[vertex_id]
-  // return this.vertices.first(function(vertex) {return vertex._id == vertex_id})
+DagobaSlow.Graph.findVertexById = function(vertex_id) {
+  return this.vertices.first(function(vertex) {return vertex._id == vertex_id})
 }
 
-Dagoba.Graph.findVerticesByIds = function(ids) {
+DagobaSlow.Graph.findVerticesByIds = function(ids) {
   return ids.length ? ids.map(this.findVertexById.bind(this)).filter(Boolean) : this.vertices.slice()
 }
 
-Dagoba.Graph.findVertices = function(ids) {
+DagobaSlow.Graph.findVertices = function(ids) {
   return !ids.length || 1+ +ids[0] ? this.findVerticesByIds(ids) : this.searchVertices(ids)
 }
 
-Dagoba.Graph.searchVertices = function(obj) {
+DagobaSlow.Graph.searchVertices = function(obj) {
   return this.vertices.filter(
     function(vertex) {
       return Object.keys(obj[0]).reduce(
         function(acc, key) {
           return acc && obj[0][key] == vertex[key] }, true ) } ) }
 
-Dagoba.Graph.findEdgeById = function(edge_id) {
+DagobaSlow.Graph.findEdgeById = function(edge_id) {
   return this.edges.first(function(edge) {return edge._id == edge_id}) }
 
-Dagoba.Graph.findOutEdges = function(vertex) {
+DagobaSlow.Graph.findOutEdges = function(vertex) {
   return this.edges.filter(function(edge) {return edge._out == vertex._id}) }
 
-Dagoba.Graph.findInEdges = function(vertex) {
+DagobaSlow.Graph.findInEdges = function(vertex) {
   return this.edges.filter(function(edge) {return edge._in == vertex._id}) }
 
-Dagoba.filterThings = function(arg) {
+DagobaSlow.filterThings = function(arg) {
   return function(thing) {
     return !arg ? true : arg+''===arg ? thing._label == arg
-                : Array.isArray(arg)  ? !!~arg.indexOf(thing._label) : Dagoba.objFilter(thing, arg) } }
+                : Array.isArray(arg)  ? !!~arg.indexOf(thing._label) : DagobaSlow.objFilter(thing, arg) } }
 
-Dagoba.objFilter = function(thing, obj) {
+DagobaSlow.objFilter = function(thing, obj) {
   for(var key in obj)
     if(thing[key] != obj[key])
       return false; return true }
@@ -305,7 +301,7 @@ Array.prototype.first = function(fun) {
     if(fun(this[i]))
       return this[i] }
 
-Dagoba.onError = function(msg) {
+DagobaSlow.onError = function(msg) {
   console.log(msg)
   return false
 }
