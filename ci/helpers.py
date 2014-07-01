7fdfd12cb49988cc48cf31ef942951ce4a9dcde7
Malini Das <mdas@mozilla.com>
2014-07-01 11:52:10 -0400

    update according to reviewer's comment

--- ci/helpers.py
+++ ci/helpers.py
@@ -1,9 +1,9 @@
 import socket
 
-def communicate(host, port, req):
+def communicate(host, port, request):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
-    s.send(req)
+    s.send(request)
     response = s.recv(1024)
     s.close()
     return response
