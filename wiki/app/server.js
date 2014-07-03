1e5a08ddc993f9c9a3e34724157dcad28c321a07
Laurie MacDougall Sookraj <lmds28@gmail.com>
2014-07-03 00:26:54 -0400

    minor: dbStore doesn't need to be assigned to exports, keep as local variable

--- wiki/app/server.js
+++ wiki/app/server.js
@@ -3,8 +3,8 @@ var express = require('express');
 var whiskers = require('whiskers');
 var passport = require('passport');
 
-exports.dbStore = require('./db_store.js')(config.dbURL, config.dbName);
-require("./passport.js")(passport, exports.dbStore);
+var dbStore = require('./db_store.js')(config.dbURL, config.dbName);
+require("./passport.js")(passport, dbStore);
 
 var app = express();
 app.use('/public', express.static(__dirname + '/public'));
@@ -16,8 +16,8 @@ app.use(express.session({ secret: '500lineswiki' }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-require("./wiki_routes.js")(app, exports.dbStore);
-require("./auth_routes.js")(app, exports.dbStore, passport);
+require("./wiki_routes.js")(app, dbStore);
+require("./auth_routes.js")(app, dbStore, passport);
 
 exports.server = app.listen(config.webserverport, function(){
     console.log("Server started. Visit http://localhost:" + config.webserverport + "/wiki/ to access the wiki.");
