07addaf13f7d859736a0b2f8f3a89e05652ffb3a
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-07-06 13:18:26 -0700

    Clean up comments

--- sampler/multinomial.py
+++ sampler/multinomial.py
@@ -19,16 +19,16 @@ class MultinomialDistribution(object):
         self.logp = np.log(self.p)
         self.rso = rso
 
-        # get the appropriate function for generating the random
+        # Get the appropriate function for generating the random
         # samples, depending on whether we're using a RandomState
-        # object or not
+        # object or not.
         if self.rso:
             self._sample_func = self.rso.multinomial
         else:
             self._sample_func = np.random.multinomial
 
-        # check that the probabilities sum to 1 -- if they don't, then
-        # something is wrong
+        # Check that the probabilities sum to 1 -- if they don't, then
+        # something is wrong.
         if not np.isclose(np.sum(self.p), 1.0):
             raise ValueError("event probabilities do not sum to 1")
 
@@ -46,7 +46,7 @@ class MultinomialDistribution(object):
         The evaluated log-PMF for draw `x`
 
         """
-        # get the total number of events
+        # Get the total number of events.
         n = np.sum(x)
 
         # equivalent to log(n!)
@@ -54,7 +54,7 @@ class MultinomialDistribution(object):
         # equivalent to log(x1! * ... * xk!)
         denominator = np.sum(gammaln(x + 1))
 
-        # if one of the values of self.p is 0, then the corresponding
+        # If one of the values of self.p is 0, then the corresponding
         # value of self.logp will be -inf. If the corresponding value
         # of x is 0, then multiplying them together will give nan, but
         # we want it to just be 0.
@@ -63,7 +63,7 @@ class MultinomialDistribution(object):
         # equivalent to log(p1^x1 * ... * pk^xk)
         weights = np.sum(all_weights)
 
-        # put it all together
+        # Put it all together.
         log_pmf = numerator - denominator + weights
         return log_pmf
 
