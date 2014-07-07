d1d33b1ed98b80e1e23c075953e2531fe534f69b
Yoav Rubin <yoavrubin@hotmail.com>
2014-07-07 22:42:37 +0300

    refactorings of the graph module

--- functionalDB/src/fdb/graph.clj
+++ functionalDB/src/fdb/graph.clj
@@ -2,19 +2,16 @@
   (:use fdb.constructs))
 
 (defn outgoing-refs [db ts ent-id]
-  (if ent-id
+  (if-not ent-id []
     (->> (entity-at db ts ent-id)
-          (:attrs) (vals) (filter ref?) (mapcat  (comp collify :value)))
-    []))
+          (:attrs) (vals) (filter ref?) (mapcat :value))))
 
 (defn incoming-refs [db ts ent-id]
   (let [vaet (ind-at db :VAET ts)]
       (reduce into #{} (vals (vaet ent-id)))))
 
 (defn- remove-explored [pendings explored restruct-fn]
-  (if (contains? explored (first pendings))
-    (recur (rest pendings) explored restruct-fn)
-      (restruct-fn pendings)))
+  (restruct-fn (remove #(contains? explored %) pendings)))
 
 (defn- traverse [pendings explored exploring-fn ent-at restruct-fn]
     (let [cleaned-pendings (remove-explored pendings explored restruct-fn)
@@ -22,12 +19,11 @@
           all-next-items  (exploring-fn item)
           next-pends (reduce conj (restruct-fn (rest cleaned-pendings)) all-next-items)]
       (when item (cons  (ent-at item)
-                                   (lazy-seq
-                                      (traverse next-pends  (conj explored item) exploring-fn ent-at restruct-fn))))))
+                                   (lazy-seq (traverse next-pends (conj explored item) exploring-fn ent-at restruct-fn))))))
 
 (defn traverse-db
   ([start-ent-id db algo direction] (traverse-db start-ent-id db algo direction (:curr-time db)))
   ([start-ent-id db algo direction ts]
-   (let [pend-struct (if (= :bfs algo) vec list*)
-         explor-fn (if (= :outgoing direction) outgoing-refs incoming-refs)]
-     (traverse [start-ent-id] #{}  (partial explor-fn db ts) (partial entity-at db ts) pend-struct))))
+   (let [pend-struct (if (= :graph/bfs algo) vec list*)
+          explore-fn (if (= :graph/outgoing direction) outgoing-refs incoming-refs)]
+     (traverse [start-ent-id] #{}  (partial explore-fn db ts) (partial entity-at db ts) pend-struct))))
