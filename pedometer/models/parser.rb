e58bf92623cc8dfc97e31d9b1b04066923b928d8
Dessy Daskalov <dessy.daskalov@gmail.com>
2014-07-24 11:28:33 -0400

    Clean up parse_raw_data and add better comments

--- pedometer/models/parser.rb
+++ pedometer/models/parser.rb
@@ -31,29 +31,34 @@ class Parser
   # You should be more explicit with your exception catching. It's better to 
   # have specific exceptions that you except to be raised, and have logic to handle those cases.
   def parse_raw_data
-    # Extract numerical data in the format:
-    # [[[x1,y1,z1], ..., [[xn,yn,zn]]]
+    # Extract numerical data into the format:
+    # [ [ [x1,y1,z1] ], ..., [ [xn,yn,zn] ] ]
     # OR
-    # [[[x1,y1,z1], [xg1,yg1,zg1], ..., [[xn,yn,zn], [xgn,ygn,zgn]]]
+    # [ [ [x1,y1,z1], [xg1,yg1,zg1] ], ..., [ [xn,yn,zn], [xgn,ygn,zgn] ] ]
     accl = @data.split(';').map { |i| i.split('|') }
                 .map { |i| i.map { |i| i.split(',').map(&:to_f) } }
     
-    # Split acceleration data into the following format:
-    # [ [ [x1, x2, ..., xn],    [y1, y2, ..., yn],    [z1, z2, ..., zn] ],
-    #   [ [xg1, xg2, ..., xgn], [yg1, yg2, ..., ygn], [zg1, zg2, ..., zgn] ] ]
     @format = if accl.first.count == 1
-      split_accl = accl.map(&:flatten).transpose.map do |total_accl|
+      # TODO: Try to combine the two loops below. Easier to explain. 
+      #       See branch parser-option-2.
+
+      # Low-pass filter combined acceleration into the following format:
+      # [ [ [x1u, x2u, ..., xnu], [x1g, x2g, ..., xng] ],
+      #   [ [y1u, y2u, ..., ynu], [y1g, y2g, ..., yng] ],
+      #   [ [z1u, z2u, ..., znu], [z1g, z2g, ..., zng] ], ]
+      filtered_accl = accl.map(&:flatten).transpose.map do |total_accl|
         grav = chebyshev_filter(total_accl, GRAVITY_COEFF)
         user = total_accl.zip(grav).map { |a, b| a - b }
         [user, grav]
       end
 
-      # TODO: Too crazy. Find shortcut.
-      accl = (0..accl.length-1).inject([]) do |a, i| 
-        coordinate_user = split_accl.map(&:first).map { |elem| elem[i] }
-        coordinate_grav = split_accl.map(&:last).map { |elem| elem[i] }
+      # Format split acceleration into the following format:
+      # [ [ [x1,y1,z1], [xg1,yg1,zg1] ], ..., [ [xn,yn,zn], [xgn,ygn,zgn] ] ]
+      accl = (0..accl.length-1).map do |i| 
+        coordinate_user = filtered_accl.map(&:first).map { |elem| elem[i] }
+        coordinate_grav = filtered_accl.map(&:last).map { |elem| elem[i] }
 
-        a << [coordinate_user, coordinate_grav]
+        [coordinate_user, coordinate_grav]
       end
 
       FORMAT_COMBINED
@@ -61,6 +66,9 @@ class Parser
       FORMAT_SEPARATED
     end
 
+    # Format accl into the following format:
+    # [ [ [x1u, x2u, ..., xnu], [y1u, y2u, ..., ynu], [z1u, z2u, ..., znu] ],
+    #   [ [x1g, x2g, ..., xng], [y1g, y2g, ..., yng], [z1g, z2g, ..., zng] ] ] ]
     split_accl = [accl.map(&:first).transpose, accl.map(&:last).transpose]
 
     user_accl, grav_accl   = split_accl
