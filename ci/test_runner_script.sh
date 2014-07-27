725926345acaeb77680407a5ac1a5debdfb6e179
Malini Das <mdas@mozilla.com>
2014-07-27 17:18:17 -0400

    use quotes

--- ci/test_runner_script.sh
+++ ci/test_runner_script.sh
@@ -4,7 +4,7 @@ COMMIT=$2
 
 source run_or_fail.sh
 
-run_or_fail "Repository folder not found" pushd $REPO
+run_or_fail "Repository folder not found" pushd "$REPO"
 run_or_fail "Could not clean repository" git clean -d -f -x
 run_or_fail "Could not call git pull" git pull
-run_or_fail "Could not update to given commit hash" git reset --hard $COMMIT
+run_or_fail "Could not update to given commit hash" git reset --hard "$COMMIT"
