439ffc3d447dbfb675c70d89091039166a412e23
Carl Friedrich Bolz <cfbolz@gmx.de>
2014-08-12 22:11:32 +0200

    add a bit to the map test

--- objmodel/04-maps/test_objmodel.py
+++ objmodel/04-maps/test_objmodel.py
@@ -244,15 +244,22 @@ def test_maps():
     p1 = Instance(Point)
     p1.write_attr("x", 1)
     p1.write_attr("y", 2)
+    assert p1.storage == [1, 2]
+    assert p1.map.attrs == {"x": 0, "y": 1}
 
     p2 = Instance(Point)
     p2.write_attr("x", 5)
     p2.write_attr("y", 6)
     assert p1.map is p2.map
-    assert p1.storage == [1, 2]
     assert p2.storage == [5, 6]
 
     p1.write_attr("x", -1)
     p1.write_attr("y", -2)
     assert p1.map is p2.map
     assert p1.storage == [-1, -2]
+
+    p3 = Instance(Point)
+    p3.write_attr("x", 100)
+    p3.write_attr("z", -343)
+    assert p3.map is not p1.map
+    assert p3.map.attrs == {"x": 0, "z": 1}
