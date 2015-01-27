0579effeaeacc7b3a51ba380aecc90ab29e164b8
Allison Kaptur <allison.kaptur@gmail.com>
2015-01-26 23:52:25 -0800

    new code and new transitions

--- interpreter/conditionals_interpreter.py
+++ interpreter/conditionals_interpreter.py
@@ -20,6 +20,10 @@ class SimpleInterpreter(object):
         total = first_num + second_num
         self.stack.append(total)
 
+    def BINARY_LESS_THAN(self):
+        a, b = self.stack.pop(), self.stack.pop()
+        self.stack.append((a < b))
+
     def STORE_NAME(self, name):
         val = self.stack.pop()
         print("storing name %s: %s" % (name, val))
@@ -28,6 +32,9 @@ class SimpleInterpreter(object):
     def LOAD_NAME(self, name):
         val = self.environment[name]
         self.stack.append(val)
+
+    def JUMP(self, jump_target):
+        self.next_i = jump_target
 
     def JUMP_IF_FALSE(self, jump_target):
         cond = self.stack.pop()
@@ -40,7 +47,7 @@ class SimpleInterpreter(object):
     def parse_argument(self, instruction, argument, what_to_execute):
         argument_meaning = {"values": ["LOAD_VALUE"],
                             "names": ["LOAD_NAME", "STORE_NAME"],
-                            "jumps": ["JUMP_IF_FALSE"]}
+                            "jumps": ["JUMP_IF_FALSE", "JUMP"]}
 
         if instruction in argument_meaning["values"]:
             argument = what_to_execute["values"][argument]
@@ -58,7 +65,10 @@ class SimpleInterpreter(object):
         while True:
             if self.next_i >= len(instructions) or self.should_stop:
                 break
+            print(self.next_i)
+            print(self.environment)
             instruction, argument = instructions[self.next_i]
+            print(instructions[self.next_i])
             argument = self.parse_argument(instruction, argument, what_to_execute)
 
             if instruction == "LOAD_VALUE":
@@ -73,30 +83,79 @@ class SimpleInterpreter(object):
                 self.LOAD_NAME(argument)
             elif instruction == "JUMP_IF_FALSE":
                 self.JUMP_IF_FALSE(argument)
+            elif instruction == "JUMP":
+                self.JUMP(argument)
             elif instruction == "RETURN":
                 self.RETURN()
 
             self.next_i += 1
 
 def test_simple_interpreter():
+    def test_conditional_true():
+        what_to_execute = {
+            "instructions": [("LOAD_VALUE", 0),
+                             ("STORE_NAME", 0),
+                             ("LOAD_NAME", 0),
+                             ("JUMP_IF_FALSE", 6),
+                             ("LOAD_VALUE", 1),
+                             ("PRINT_ANSWER", None),
+                             ("RETURN", None),
+                             ("LOAD_VALUE", 2),
+                             ("PRINT_ANSWER", None),
+                             ("RETURN", None)],
+            "values": [True, 'yes', 'no'],
+            "names": ["a"] }
 
-    what_to_execute = {
-        "instructions": [("LOAD_VALUE", 0),
-                         ("STORE_NAME", 0),
-                         ("LOAD_NAME", 0),
-                         ("JUMP_IF_FALSE", None),
-                         ("LOAD_VALUE", 1),
-                         ("PRINT_ANSWER", None),
-                         ("RETURN", None),
-                         ("LOAD_VALUE", 2),
-                         ("PRINT_ANSWER", None),
-                         ("RETURN", None)],
-        "values": [True, 'yes', 'no'],
-        "names": ["a"] }
+        interpreter = SimpleInterpreter()
+        interpreter.execute(what_to_execute)
+        print("== 'yes'")
+    test_conditional_true()
 
-    interpreter = SimpleInterpreter()
-    interpreter.execute(what_to_execute)
+    def test_conditional_false():
+        what_to_execute = {
+            "instructions": [("LOAD_VALUE", 0),
+                             ("STORE_NAME", 0),
+                             ("LOAD_NAME", 0),
+                             ("JUMP_IF_FALSE", 6),
+                             ("LOAD_VALUE", 1),
+                             ("PRINT_ANSWER", None),
+                             ("RETURN", None),
+                             ("LOAD_VALUE", 2),
+                             ("PRINT_ANSWER", None),
+                             ("RETURN", None)],
+            "values": [False, 'yes', 'no'],
+            "names": ["a"] }
+        interpreter = SimpleInterpreter()
+        interpreter.execute(what_to_execute)
+        print("== 'no'")
+    test_conditional_false()
 
+    # def test_loop():
+        # >>> def loop():
+        #         x = 1
+        # ...     while x < 5:
+        # ...         print("going")
+        # what_to_execute = {
+        #     "instructions": [("LOAD_VALUE", 0),
+        #                      ("STORE_NAME", 0),
+        #                      ("LOAD_NAME", 0),
+        #                      ("LOAD_VALUE", 1),
+        #                      ("BINARY_LESS_THAN", None),
+        #                      ("JUMP_IF_FALSE", 8), # <- decide whether or not to jump FIXME
+        #                      ("LOAD_VALUE", 2),
+        #                      ("PRINT_ANSWER", None),
+        #                      ("LOAD_NAME", 0),
+        #                      ("LOAD_VALUE", 1),
+        #                      ("ADD_TWO_VALUES", None),
+        #                      ("STORE_NAME", 0),
+        #                      ("JUMP", 1),        # <- Jump to top of loop
+        #                      ("RETURN", None)],  # <- Jump to here when done looping
+        #     "values": [1, 5, 'going'],
+        #     "names": ["x"] }
+        # interpreter = SimpleInterpreter()
+        # interpreter.execute(what_to_execute)
+        # print("==" + "going\n"*5)
+    # test_loop()
 
 if __name__ == '__main__':
     test_simple_interpreter()
