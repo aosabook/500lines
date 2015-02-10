5460ce969a06167873865f5fd83caf0afac7ff0c
Dessy Daskalov <dessy.daskalov@gmail.com>
2015-02-09 21:41:23 -0500

    Spacing

--- pedometer/models/filter.rb
+++ pedometer/models/filter.rb
@@ -5,11 +5,11 @@ class Filter
     beta:  [0.000086384997973502, 0.000172769995947004, 0.000086384997973502]
   }
   COEFFICIENTS_LOW_5_HZ = { # Direct form I, Chebyshev II, type = low-pass, Astop = 2, Fstop = 5, Fs = 100, Direct Form I
-    alpha: [1, -1.80898117793047, 0.827224480562408], 
+    alpha: [1, -1.80898117793047, 0.827224480562408],
     beta:  [0.095465967120306, -0.172688631608676, 0.095465967120306]
   }
-  COEFFICIENTS_HIGH_1_HZ = { # Direct form I, Chebyshev II, type = high-pass, Fs = 100, Fstop = 0.5, Astop = 20, order = 2, 
-    alpha: [1, -1.905384612118461, 0.910092542787947], 
+  COEFFICIENTS_HIGH_1_HZ = { # Direct form I, Chebyshev II, type = high-pass, Fs = 100, Fstop = 0.5, Astop = 20, order = 2,
+    alpha: [1, -1.905384612118461, 0.910092542787947],
     beta:  [0.953986986993339, -1.907503180919730, 0.953986986993339]
   }
 
@@ -30,7 +30,7 @@ private
   def self.filter(data, coefficients)
     filtered_data = [0,0]
     (2..data.length-1).each do |i|
-      filtered_data << coefficients[:alpha][0] * 
+      filtered_data << coefficients[:alpha][0] *
                       (data[i]            * coefficients[:beta][0] +
                        data[i-1]          * coefficients[:beta][1] +
                        data[i-2]          * coefficients[:beta][2] -
