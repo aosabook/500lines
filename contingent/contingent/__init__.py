d181b290f707de101a0da7b01d6b9a0c8423b095
Brandon Rhodes <brandon@rhodesmill.org>
2015-05-11 18:30:54 -0400

    Rewrite the Contingent docstring

--- contingent/contingent/__init__.py
+++ contingent/contingent/__init__.py
@@ -1,17 +1,23 @@
 """Contingent: a build system that responds to changes with minimal rebuilding.
 
-Many build systems respond to an edited input file by either doing too
-much, like recompiling your entire project, or rebuilding a whole
-collection of documents.  Others do too little, regenerating only the
-most obviously affected output files but missing subtle ways in which
-other build products also need to be regenerated.
+Most build systems allow only absolute rules, like "If ``aux.c`` has
+been modified, then *always* rebuild ``aux.o``."  But when documents are
+built from a collection of files that contain cross references, absolute
+rules are overly limiting.
 
-Contingent allows a programmer to describe a build process as a network
-of Python function calls.  Contingent will automatically learn the
-dependencies between these calls; re-run every necessary downstream step
-when an input file changes; prevent a routine from re-running if none of
-its inputs has changed; and re-learn the inputs to a step that is re-run
-in case cross references between resources are added or deleted in the
-course of a project's history.
+Contingent allows a programmer to instead describe a build process as a
+network of Python function calls, that each describe one task.  The
+return values alone determine whether a given downstream task needs to
+be rebuilt.  Contingent will:
+
+* Automatically learn the dependencies between tasks.
+
+* Re-run every downstream task when an upstream output changes.
+
+* Prevent a routine from re-running if none of its inputs has changed.
+
+* Re-learn the inputs to a step that is re-run, in case dependencies
+  between resources are added or deleted in the course of a project's
+  history.
 
 """
