9141d57939b1e67eeab44fbde6555ffd373eb2c2
Michael DiBernardo <mikedebo@gmail.com>
2015-05-30 10:27:28 -0400

    Move csl

--- build.py
+++ build.py
@@ -156,7 +156,7 @@ def build_mobi():
 def build_html(chapter_markdowns):
     run('mkdir -p html/content/pages')
     temp = 'python _build/preprocessor.py --chapter {chap} --html-refs --html-paths --output={md}.1 --latex {md}'
-    temp2 = 'pandoc --csl=ieee.csl --bibliography=tex/500L.bib -t html -f markdown+citations -o html/content/pages/{basename}.md {md}.1'
+    temp2 = 'pandoc --csl=minutiae/ieee.csl --bibliography=tex/500L.bib -t html -f markdown+citations -o html/content/pages/{basename}.md {md}.1'
     temp3 = './_build/fix_html_title.sh html/content/pages/{basename}.md'
     for i, markdown in enumerate(chapter_markdowns):
         basename = os.path.splitext(os.path.split(markdown)[1])[0]
