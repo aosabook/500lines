a1329b82ae90f0be7c47b9d84dfe00218641ecc1
Michael DiBernardo <mikedebo@gmail.com>
2015-09-11 07:20:41 -0400

    Project format.

--- build.py
+++ build.py
@@ -14,6 +14,7 @@ def main(chapters=[], epub=False, pdf=False, html=False, mobi=False, pandoc_epub
             run('rm {}'.format(f))
 
     chapter_dirs = [
+        'static-analysis',
         'flow-shop',
         'template-engine',
         'pedometer',
